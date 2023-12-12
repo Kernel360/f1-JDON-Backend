7ca78076a04d40febf6eb06174abe6dc5bc95147
aqrms <aquarimus@gmail.com>
2023-12-12 21:48:59 +0900

    feat: FAQ 삭제 기능 추가

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/faq/controller/FaqController.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/faq/controller/FaqController.java
@@ -1,6 +1,7 @@
 package kernel.jdon.moduleapi.domain.faq.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 import kernel.jdon.moduleapi.domain.faq.dto.FindFaqResponse;
 import kernel.jdon.moduleapi.domain.faq.service.FaqService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 public class FaqController {
 	private final FaqService faqService;
 
 	@GetMapping("/api/v1/faqs/{faqId}")
-	public ResponseEntity<FindFaqResponse> get(@PathVariable Long faqId) {
+	public ResponseEntity<FindFaqResponse> get(@PathVariable(name = "faqId") Long faqId) {
 		FindFaqResponse findFaqResponse = faqService.find(faqId);
 
 		return ResponseEntity.ok().body(findFaqResponse);
 	}
+
+	@DeleteMapping("/api/v1/faqs/{faqId}")
+	public ResponseEntity<Long> remove(@PathVariable(name = "faqId") Long faqId) {
+		faqService.delete(faqId);
+		return ResponseEntity.ok(faqId);
+	}
+
 }
--- module-api/src/main/java/kernel/jdon/moduleapi/domain/faq/service/FaqService.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/faq/service/FaqService.java
@@ -1,11 +1,14 @@
 package kernel.jdon.moduleapi.domain.faq.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import kernel.jdon.moduleapi.domain.faq.dto.FindFaqResponse;
 import kernel.jdon.moduleapi.domain.faq.entity.Faq;
 import kernel.jdon.moduleapi.domain.faq.repository.FaqRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +24,11 @@ public class FaqService {
 	private Faq findById(Long faqId) {
 		return faqRepository.findById(faqId)
 			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Faq 입니다."));
+	}
+
+	@Transactional
+	public void delete(Long faqId) {
+		findById(faqId);
+		faqRepository.deleteById(faqId);
 	}
 }
