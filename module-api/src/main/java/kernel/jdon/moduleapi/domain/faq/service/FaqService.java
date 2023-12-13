a4cc2e9855e14152b3093d8994f3b81d1d3598b4
김현지 <86637372+aacara@users.noreply.github.com>
2023-12-13 12:49:46 +0900

    Feature: FAQ 등록 (#14)
    * [Feature] #5. FAQ 등록
    
    - FAQ 등록 api 생성
    
    * Refactor: #5. FAQ 등록
    
    - FAQ 등록 api 생성 코드리뷰 리팩토링
    
    * Refactor: indent 수정
    
    indent 수정
    
    * Refactor: #5. FAQ ì°파일 충돌 해결
    
    reques- t, response 디렉토리 변경 -ㅑindent 수정 -print문 제거

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/faq/service/FaqService.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/faq/service/FaqService.java
@@ -1,16 +1,19 @@
 package kernel.jdon.moduleapi.domain.faq.service;
 
-import kernel.jdon.moduleapi.domain.faq.dto.request.ModifyFaqRequest;
-import kernel.jdon.moduleapi.domain.faq.dto.response.ModifyFaqResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import kernel.jdon.moduleapi.domain.faq.dto.FindFaqResponse;
+import kernel.jdon.moduleapi.domain.faq.dto.request.CreateFaqRequest;
+import kernel.jdon.moduleapi.domain.faq.dto.request.ModifyFaqRequest;
+import kernel.jdon.moduleapi.domain.faq.dto.response.CreateFaqResponse;
+import kernel.jdon.moduleapi.domain.faq.dto.response.FindFaqResponse;
+import kernel.jdon.moduleapi.domain.faq.dto.response.ModifyFaqResponse;
 import kernel.jdon.moduleapi.domain.faq.entity.Faq;
 import kernel.jdon.moduleapi.domain.faq.repository.FaqRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class FaqService {
 	private final FaqRepository faqRepository;
@@ -24,6 +27,13 @@ public class FaqService {
 	private Faq findById(Long faqId) {
 		return faqRepository.findById(faqId)
 			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Faq 입니다."));
+	}
+
+	@Transactional
+	public CreateFaqResponse create(CreateFaqRequest createFaqRequest) {
+		Faq savedFaq = faqRepository.save(CreateFaqRequest.toEntity(createFaqRequest));
+
+		return CreateFaqResponse.of(savedFaq);
 	}
 
 	@Transactional
