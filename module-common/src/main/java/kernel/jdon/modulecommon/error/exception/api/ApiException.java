395495d29798169e902034293bd9c73dde5a00ab
이윤선 <yoonseon3@gmail.com>
2023-12-18 21:17:38 +0900

    Refactor: transient 키워드 적용

--- module-common/src/main/java/kernel/jdon/modulecommon/error/exception/api/ApiException.java
+++ module-common/src/main/java/kernel/jdon/modulecommon/error/exception/api/ApiException.java
@@ -3,7 +3,7 @@ package kernel.jdon.modulecommon.error.exception.api;
 import kernel.jdon.modulecommon.error.code.ErrorCode;
 
 public class ApiException extends RuntimeException {
-	private ErrorCode errorCode;
+	private transient ErrorCode errorCode;
 
 	public ApiException(ErrorCode errorCode) {
 		this.errorCode = errorCode;
