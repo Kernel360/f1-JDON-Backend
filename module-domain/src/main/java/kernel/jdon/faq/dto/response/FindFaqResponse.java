9b9c6437ab1a6af29441dc513a0c903fbf3a8522
anso33 <hyeon131812@gmail.com>
2023-12-19 17:28:48 +0900

    Fix: Add faqId at FindFaqListResponse

--- module-domain/src/main/java/kernel/jdon/faq/dto/response/FindFaqResponse.java
+++ module-domain/src/main/java/kernel/jdon/faq/dto/response/FindFaqResponse.java
@@ -9,11 +9,13 @@ import lombok.Getter;
 @Builder
 @AllArgsConstructor
 public class FindFaqResponse {
+	private Long faqId;
 	private String title;
 	private String content;
 
 	public static FindFaqResponse of(Faq faq) {
 		return FindFaqResponse.builder()
+			.faqId(faq.getId())
 			.title(faq.getTitle())
 			.content(faq.getContent())
 			.build();
