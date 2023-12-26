e4748bca36dcf5a5640dc39d993bf11b114c0214
이윤선 <yoonseon3@gmail.com>
2023-12-26 12:24:39 +0900

    Feature: module-crawler 생성

---
+++ module-crawler/src/main/java/kernel/jdon/ModuleCrawlerApplication.java
@@ -0,0 +1,13 @@
+package kernel.jdon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleCrawlerApplication.class, args);
+	}
+
+}
