5859e34a8b8c2d2e75434df2b76f7834c6083edf
aacara <budgielover@naver.com>
2023-12-29 15:39:03 +0900

    [BugFix] #60. connect inflearn_course with wanted_jd_skill

---
+++ module-domain/src/main/java/kernel/jdon/inflearnJdskill/domain/InflearnJdSkill.java
@@ -0,0 +1,29 @@
+package kernel.jdon.inflearnJdskill.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import kernel.jdon.inflearn.domain.InflearnCourse;
+import kernel.jdon.wantedskill.domain.WantedJdSkill;
+
+@Entity
+@Table(name = "inflearn_jd_skill")
+public class InflearnJdSkill {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "wanted_jd_skill_id", columnDefinition = "BIGINT")
+	private WantedJdSkill wantedJdSkill;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "course_id", columnDefinition = "BIGINT")
+	private InflearnCourse inflearnCourse;
+}
