69901cd7164d46379022e89625851e5933392e5b
이윤선 <yoonseon3@gmail.com>
2024-01-04 21:28:09 +0900

    fix: Exception.class 예외처리 제거

--- module-common/src/main/java/kernel/jdon/error/exception/GlobalExceptionHandler.java
+++ module-common/src/main/java/kernel/jdon/error/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package kernel.jdon.error.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,13 +10,6 @@ import kernel.jdon.error.exception.api.ApiException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> handlerException(Exception e, HttpServletRequest request) {
-		final HttpStatus internalServerError = HttpStatus.INTERNAL_SERVER_ERROR;
-		return ResponseEntity.status(internalServerError.value())
-			.body(ErrorResponse.of(internalServerError, request));
-	}
 
 	@ExceptionHandler(ApiException.class)
 	public ResponseEntity<ErrorResponse> handlerApiException(ApiException e, HttpServletRequest request) {
