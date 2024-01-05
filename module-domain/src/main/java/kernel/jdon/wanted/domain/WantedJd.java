b14eb9fdf3aaf46097b6f9f5d0aa030158971a26
이윤선 <yoonseon3@gmail.com>
2024-01-05 13:45:45 +0900

    BugFix: add column title, scrapingDate at WantedJd table (#84)

--- module-domain/src/main/java/kernel/jdon/wanted/domain/WantedJd.java
+++ module-domain/src/main/java/kernel/jdon/wanted/domain/WantedJd.java
@@ -1,7 +1,11 @@
 package kernel.jdon.wanted.domain;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "wanted_jd", uniqueConstraints = @UniqueConstraint(columnNames = {"detail_id", "job_category_id"}))
 public class WantedJd {
 
@@ -29,6 +34,9 @@ public class WantedJd {
 	@Column(name = "company_name", columnDefinition = "VARCHAR(50)", nullable = false)
 	private String companyName;
 
+	@Column(name = "title", columnDefinition = "VARCHAR(255)", nullable = false)
+	private String title;
+
 	@Column(name = "detail_id", columnDefinition = "BIGINT", nullable = false)
 	private Long detailId;
 
@@ -38,16 +46,24 @@ public class WantedJd {
 	@Column(name = "image_url", columnDefinition = "TEXT", nullable = false)
 	private String imageUrl;
 
+	@CreatedDate
+	@Column(name = "scraping_date", columnDefinition = "TEXT", nullable = false)
+	private String scrapingDate;
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "job_category_id", columnDefinition = "BIGINT")
 	private JobCategory jobCategory;
 
 	@Builder
-	public WantedJd(String companyName, Long detailId, String detailUrl, String imageUrl, JobCategory jobCategory) {
+	public WantedJd(String companyName, String title, Long detailId, String detailUrl, String imageUrl,
+		String scrapingDate,
+		JobCategory jobCategory) {
 		this.companyName = companyName;
+		this.title = title;
 		this.detailId = detailId;
 		this.detailUrl = detailUrl;
 		this.imageUrl = imageUrl;
+		this.scrapingDate = scrapingDate;
 		this.jobCategory = jobCategory;
 	}
 }
