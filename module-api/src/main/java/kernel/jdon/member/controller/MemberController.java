202d786b04d5024b23c321db6204c77334706a6b
anso33 <hyeon131812@gmail.com>
2024-01-05 12:25:49 +0900

    Feature: Add mock-api for get member info

--- module-api/src/main/java/kernel/jdon/member/controller/MemberController.java
+++ module-api/src/main/java/kernel/jdon/member/controller/MemberController.java
@@ -1,6 +1,10 @@
 package kernel.jdon.member.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import kernel.jdon.dto.response.CommonResponse;
 import kernel.jdon.member.dto.request.ModifyMemberRequest;
 import kernel.jdon.member.dto.request.SaveMemberRequest;
+import kernel.jdon.member.dto.response.GetMemberResponse;
 import kernel.jdon.member.dto.response.ModifyMemberResponse;
 import kernel.jdon.member.dto.response.SaveMemberResponse;
 
@@ -22,9 +27,23 @@ public class MemberController {
 		return ResponseEntity.ok(CommonResponse.of(SaveMemberResponse.of(1L)));
 	}
 
+	@GetMapping("/member")
+	public ResponseEntity<CommonResponse> get() {
+		GetMemberResponse getMemberResponse = GetMemberResponse.builder()
+			.email("aaa@gmail.com")
+			.nickname("aaa")
+			.birth(LocalDate.now())
+			.gender("여성")
+			.jobCategoryId(1L)
+			.skillList(List.of(1L, 2L, 3L))
+			.build();
+		return ResponseEntity.ok(CommonResponse.of(getMemberResponse));
+	}
+
 	@PutMapping("/member")
 	public ResponseEntity<CommonResponse> modify(@RequestBody ModifyMemberRequest modifyMemberRequest) {
-		ModifyMemberResponse modifyMemberResponse = ModifyMemberResponse.builder().email(modifyMemberRequest.getEmail())
+		ModifyMemberResponse modifyMemberResponse = ModifyMemberResponse.builder()
+			.email(modifyMemberRequest.getEmail())
 			.nickname(modifyMemberRequest.getNickname())
 			.birth(modifyMemberRequest.getBirth())
 			.gender(modifyMemberRequest.getGender())
