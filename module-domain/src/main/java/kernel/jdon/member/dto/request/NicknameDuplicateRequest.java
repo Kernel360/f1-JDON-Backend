990435635049c17c7633cb879158c787142d5f8a
anso33 <hyeon131812@gmail.com>
2024-01-05 13:39:32 +0900

    Refactor: Add private default constructor

--- module-domain/src/main/java/kernel/jdon/member/dto/request/NicknameDuplicateRequest.java
+++ module-domain/src/main/java/kernel/jdon/member/dto/request/NicknameDuplicateRequest.java
@@ -1,8 +1,11 @@
 package kernel.jdon.member.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class NicknameDuplicateRequest {
 	private String nickname;
 }
--- module-domain/src/main/java/kernel/jdon/member/dto/request/SaveMemberRequest.java
+++ module-domain/src/main/java/kernel/jdon/member/dto/request/SaveMemberRequest.java
@@ -3,9 +3,12 @@ package kernel.jdon.member.dto.request;
 import java.time.LocalDate;
 import java.util.List;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SaveMemberRequest {
 	private String email;
 	private String nickname;
