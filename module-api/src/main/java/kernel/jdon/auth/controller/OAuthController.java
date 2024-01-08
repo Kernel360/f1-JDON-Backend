97b9165bfc3dd108a4bda86632cf72233b07e68c
anso33 <hyeon131812@gmail.com>
2024-01-08 12:11:13 +0900

    Feature: Get user email from kakao oauth server

---
+++ module-api/src/main/java/kernel/jdon/auth/controller/OAuthController.java
@@ -0,0 +1,13 @@
+package kernel.jdon.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OAuthController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
