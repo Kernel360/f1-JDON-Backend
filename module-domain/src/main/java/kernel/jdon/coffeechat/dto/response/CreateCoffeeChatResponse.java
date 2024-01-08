934a5348248f2b3fece7892860ca910f25998408
aqrms <111513287+aqrms@users.noreply.github.com>
2024-01-08 16:20:07 +0900

    Feature: CoffeeChat 관련 가데이터 응답 API (#96)
    * Feat: 커피챗 목록조회 가데이터 api
    
    * Feat: 커피챗 호스트와 전체 목록 조회 가데이터 API
    
    * Feat: 전체 목록 조회 닉네임 구분
    
    * Feat: 날짜 포맷 지정을 위한 의존성 추가 및 포맷적용
    JSON 타입 데이터를 원하는 포맷으로 관리하기 위해서는 Jackson 라이브러리가 필요
    starter-web에 기본적으로 포함되어 있으나 도메인 모듈을 해당 의존성이 없으므로 별도로 추가함
    
    * Feat: createdDate 칼럼에 날짜 포맷적용
    
    * Feat: 커피챗 수정 가데이터 응답
    
    * Feat: 커피챗 삭제 가데이터 응답
    
    * Feat: 커피챗 신청 가데이터 응답
    
    * Feat: CoffeeChat 조회 DTO에 createdDate 항목 추가
    
    * Feat: CoffeeChat 컨트롤러 PathVariable 맵핑위해서 name 속성 추가
    
    * Fix: 응답 DTO에 접근제어자 어노테이션 삭제

--- module-domain/src/main/java/kernel/jdon/coffeechat/dto/response/CreateCoffeeChatResponse.java
+++ module-domain/src/main/java/kernel/jdon/coffeechat/dto/response/CreateCoffeeChatResponse.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class CreateCoffeeChatResponse {
 	private Long coffeeChatId;
