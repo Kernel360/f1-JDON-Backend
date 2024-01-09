656f451d5e247cd429a8362e60fc5db7b8b47627
aqrms <111513287+aqrms@users.noreply.github.com>
2024-01-09 17:23:19 +0900

    Feature: 커피챗 수정 API 구현 (#100)
    * Settings: Domain모듈에서 Common모듈 의존성 추가
    도메인 엔터티내에 비즈니스 로직을 수행하기위해 공용 ApiException 및 ErrorCode 필요
    
    * Feat: CoffeeChat 엔터티 내 업데이트 로직 추가
    - 커피챗 모집인원 유효성 검증
    - 커피챗 일자 유효성 검증
    - 커피챗 모집인원에 따른 상태 변경
    
    * Feat: CoffeeChat ErrorCode 추가
    
    * Feat: CoffeeChat 수정 API Controller,Service 작성
    
    * Chore: 에러코드 패키지 참조 경로 수정
    
    * Refactor: findById 메서드 분리
    
    * Refactor: 상태 업데이트 메서드 분리
    
    * Refactor: 메서드 분리 롤백

--- module-common/src/main/java/kernel/jdon/error/code/api/CoffeeChatErrorCode.java
+++ module-common/src/main/java/kernel/jdon/error/code/api/CoffeeChatErrorCode.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum CoffeeChatErrorCode implements ErrorCode {
-	NOT_FOUND_COFFEECHAT(HttpStatus.NOT_FOUND, "존재하지 않는 커피챗 입니다.");
+	NOT_FOUND_COFFEECHAT(HttpStatus.NOT_FOUND, "존재하지 않는 커피챗 입니다."),
+	INVALID_RECRUIT_COUNT(HttpStatus.BAD_REQUEST, "유효하지 않은 모집인원 입니다."),
+	EXPIRED_COFFEECHAT(HttpStatus.BAD_REQUEST, "지난 일자의 커피챗은 수정할 수 없습니다."),
+	MEET_DATE_ISBEFORE_NOW(HttpStatus.BAD_REQUEST, "지금보다 이전 시점으로 설정할 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
