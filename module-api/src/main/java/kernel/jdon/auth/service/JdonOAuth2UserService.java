489e945ff3e3cf0d64af8a2355e996ca9c3d9253
anso33 <hyeon131812@gmail.com>
2024-01-09 14:03:55 +0900

    Feature: Add OAuth login using oauth2-client library

--- module-api/src/main/java/kernel/jdon/auth/service/JdonOAuth2UserService.java
+++ module-api/src/main/java/kernel/jdon/auth/service/JdonOAuth2UserService.java
@@ -5,27 +5,55 @@ import java.util.Map;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.servlet.http.HttpSession;
+import kernel.jdon.auth.JdonOAuth2User;
+import kernel.jdon.auth.TemporaryUser;
+import kernel.jdon.member.domain.SocialProviderType;
+import kernel.jdon.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class JdonOAuth2UserService extends DefaultOAuth2UserService {
 
-	@Override
-	// 리소스 서버에서 보내주는 사용자 정보를 불러오는 메서드 loadUser
-	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-		log.info("userRequest: {}", userRequest.getAccessToken().getTokenValue());
-		OAuth2User user = super.loadUser(userRequest);
-		log.info("user: {}", user.getAttributes());
+	private final MemberRepository memberRepository;
+	private final HttpSession httpSession;
 
-		Map<String, Object> attributes = user.getAttributes();
-		for (Map.Entry<String, Object> entry : attributes.entrySet()) {
-			log.info("key: {}, value: {}", entry.getKey(), entry.getValue());
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		OAuth2User user = super.loadUser(userRequest);
+		SocialProviderType socialProvider = SocialProviderType.valueOf((
+			userRequest.getClientRegistration().getRegistrationId()).toUpperCase());
+		String userNameAttributeName = userRequest.getClientRegistration()
+			.getProviderDetails()
+			.getUserInfoEndpoint()
+			.getUserNameAttributeName();
+		if (SocialProviderType.KAKAO == socialProvider) {
+			return getEmailFromKakao(user, userNameAttributeName);
 		}
 
 		return user;
+	}
+
+	private DefaultOAuth2User getEmailFromKakao(OAuth2User user, String userNameAttributeName) {
+		Map<String, Object> attributes = user.getAttributes();
+		String email = ((Map<String, String>)attributes.get("kakao_account")).get("email");
+
+		if (memberRepository.existsByEmail(email)) {
+			httpSession.setAttribute("USER", email);
+		} else {
+			httpSession.setAttribute("GUEST", new TemporaryUser(email, SocialProviderType.KAKAO));
+		}
+
+		return new JdonOAuth2User(user.getAuthorities(), attributes, userNameAttributeName, email,
+			SocialProviderType.KAKAO);
 	}
 }
