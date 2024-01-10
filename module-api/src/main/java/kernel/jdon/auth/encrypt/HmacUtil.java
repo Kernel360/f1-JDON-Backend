f6f882168df099321032c647f93812f985b09fd5
anso33 <hyeon131812@gmail.com>
2024-01-10 21:21:45 +0900

    Feature: Add encrpt email&provider info by HMAC algorithm to digital signature

---
+++ module-api/src/main/java/kernel/jdon/auth/encrypt/HmacUtil.java
@@ -0,0 +1,30 @@
+package kernel.jdon.auth.encrypt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class HmacUtil {
+	private static final String secretKey = SecretKeyConstants.HMAC_PRIVATE_KEY;
+
+	public static String generateHMAC(String data) throws Exception {
+		Mac hmac = Mac.getInstance("HmacSHA256");
+		SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8),
+			"HmacSHA256");
+		hmac.init(secretKeySpec);
+		byte[] hmacBytes = hmac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+
+		return Base64.getEncoder().encodeToString(hmacBytes);
+	}
+
+	public static boolean validateHMAC(String receivedHMAC, String data) throws Exception {
+		String calculatedHMAC = generateHMAC(data);
+		
+		return receivedHMAC.equals(calculatedHMAC);
+	}
+}
--- module-api/src/main/java/kernel/jdon/auth/encrypt/SecretKeyConstants.java
+++ module-api/src/main/java/kernel/jdon/auth/encrypt/SecretKeyConstants.java
@@ -1,6 +1,7 @@
 package kernel.jdon.auth.encrypt;
 
 public final class SecretKeyConstants {
-	public static final String AES_PRIVATE_KEY = "JDON_SECRET_KEY_THIS_IS_USED_AES";
+	public static final String AES_PRIVATE_KEY = "JDON_SECRET_KEY_AES_USE_THIS_KEY";
+	public static final String HMAC_PRIVATE_KEY = "JDON_SECRET_KEY_HMAC_USE_THIS_KK";
 
 }
