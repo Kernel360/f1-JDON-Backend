e7b70db2fefe63ce3eb9d3017e7633d5a03ab705
이윤선 <yoonseon3@gmail.com>
2024-01-10 12:38:59 +0900

    Setting: LOG 설정 (#112)

--- module-common/src/main/java/kernel/jdon/error/exception/GlobalExceptionHandler.java
+++ module-common/src/main/java/kernel/jdon/error/exception/GlobalExceptionHandler.java
@@ -7,12 +7,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import jakarta.servlet.http.HttpServletRequest;
 import kernel.jdon.dto.response.ErrorResponse;
 import kernel.jdon.error.exception.api.ApiException;
+import kernel.jdon.error.exception.crawler.CrawlerException;
+import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ApiException.class)
 	public ResponseEntity<ErrorResponse> handlerApiException(ApiException e, HttpServletRequest request) {
+		log.warn(e.getErrorCode().getMessage(), e);
+		return ResponseEntity.status(e.getErrorCode().getHttpStatus().value())
+			.body(ErrorResponse.of(e.getErrorCode(), request));
+	}
+
+	@ExceptionHandler(CrawlerException.class)
+	public ResponseEntity<ErrorResponse> handlerApiException(CrawlerException e, HttpServletRequest request) {
+		log.warn(e.getErrorCode().getMessage(), e);
 		return ResponseEntity.status(e.getErrorCode().getHttpStatus().value())
 			.body(ErrorResponse.of(e.getErrorCode(), request));
 	}
---
+++ module-common/src/main/java/kernel/jdon/log/MdcPreference.java
@@ -0,0 +1,9 @@
+package kernel.jdon.log;
+
+public enum MdcPreference {
+	REQUEST_ID,
+	REQUEST_METHOD,
+	REQUEST_URI,
+	REQUEST_TIME,
+	REQUEST_IP
+}
