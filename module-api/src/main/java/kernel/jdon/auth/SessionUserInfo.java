f7df55072c8055af984178e9c91269f87eb79523
anso33 <hyeon131812@gmail.com>
2024-01-12 14:00:55 +0900

    Feature: Add class to save userinfo in httpsession

---
+++ module-api/src/main/java/kernel/jdon/auth/SessionUserInfo.java
@@ -0,0 +1,28 @@
+package kernel.jdon.auth;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import kernel.jdon.member.domain.Member;
+import kernel.jdon.member.domain.MemberRole;
+import lombok.Getter;
+
+@Getter
+public class SessionUserInfo implements Serializable {
+
+	private Long id;
+	private String email;
+	private LocalDateTime lastLoginDate;
+	private MemberRole role;
+
+	private SessionUserInfo(Member member) {
+		this.id = member.getId();
+		this.email = member.getEmail();
+		this.lastLoginDate = member.getLastLoginDate();
+		this.role = member.getRole();
+	}
+
+	public static SessionUserInfo of(Member member) {
+		return new SessionUserInfo(member);
+	}
+}
