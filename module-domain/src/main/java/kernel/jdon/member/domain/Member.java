642fa13383395adf2b7ef2dfd2c335b4b998f699
AN-SOHYEON <hyeon131812@gmail.com>
2024-01-12 18:13:46 +0900

    Feature: oauth-client를 이용한 OAuth2로부터 유저 정보 받아오는 로직 구현 (#106)
    * Feature: Add enum-type of social-provider on Member Entity
    
    * Feature: Add get user email from kakao oauth
    
    * Feature: Add login page
    
    * Chore: Add application.yml properties for oauth2-client setting
    
    * Feature: Get user email from kakao oauth server
    
    * Fix: Move file from member domain to auth domain
    
    * Fix: Move controller from member to auth
    
    * Feature: Add OAuth login using oauth2-client library
    
    * Fix: Add real login in register API
    
    * Fix: Add session timeout setting
    
    * Chore: Delete useless file & dependency
    
    * Fix: Modify ErrorCode to use member domain
    
    * Fix: Modify code to check that email is valid and come from right social provider
    
    * Feature: Add encrpt email&provider info by AES algorithm
    
    * Feature: Add encrpt email&provider info by HMAC algorithm to digital signature
    
    * Fix: Add compare Hmac & decode encryptString for get user email and provider
    
    * Refactor: Modify repository location to fit convention
    
    * Fix: Delete useless comment
    
    * Fix: Use util class & log method
    
    * Fix: Seperate method
    
    * Fix: Modify variable name & delete final keyword
    
    * Feature: Add Errorcode about auth
    
    * Fix: Modify csrf option in security config
    
    * Fix: Add code to save memberskill in DB
    
    * Chore: Delete Qfile

--- module-domain/src/main/java/kernel/jdon/member/domain/Member.java
+++ module-domain/src/main/java/kernel/jdon/member/domain/Member.java
@@ -20,9 +20,9 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import kernel.jdon.coffeechat.domain.CoffeeChat;
 import kernel.jdon.coffeechatmember.domain.CoffeeChatMember;
+import kernel.jdon.favorite.domain.Favorite;
 import kernel.jdon.jobcategory.domain.JobCategory;
 import kernel.jdon.memberskill.domain.MemberSkill;
-import kernel.jdon.favorite.domain.Favorite;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -34,64 +34,50 @@ import lombok.NoArgsConstructor;
 @Table(name = "member")
 public class Member {
 
+	@OneToMany(mappedBy = "member")
+	private List<CoffeeChat> hostChatList = new ArrayList<>();
+	@OneToMany(mappedBy = "member")
+	private List<CoffeeChatMember> guestChatList = new ArrayList<>();
+	@OneToMany(mappedBy = "member")
+	private List<MemberSkill> memberSkillList = new ArrayList<>();
+	@OneToMany(mappedBy = "member")
+	private List<Favorite> favoriteList = new ArrayList<>();
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-
 	@Column(name = "email", columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
 	private String email;
-
 	@Column(name = "nickname", columnDefinition = "VARCHAR(255)", nullable = false, unique = true)
 	private String nickname;
-
 	@Column(name = "birth", columnDefinition = "VARCHAR(11)", nullable = false)
 	private String birth;
-
 	@Enumerated(EnumType.STRING)
 	@Column(name = "gender", columnDefinition = "VARCHAR(11)", nullable = false)
 	private Gender gender;
-
 	@CreatedDate
 	@Column(name = "join_date", columnDefinition = "DATETIME", nullable = false)
 	private LocalDateTime joinDate;
-
 	@Column(name = "last_login_date", columnDefinition = "DATETIME")
 	private LocalDateTime lastLoginDate;
-
 	@Enumerated(EnumType.STRING)
 	@Column(name = "role", columnDefinition = "VARCHAR(10)", nullable = false)
 	private MemberRole role;
-
 	@Enumerated(EnumType.STRING)
 	@Column(name = "account_status", columnDefinition = "VARCHAR(10)", nullable = false)
 	private MemberAccountStatus accountStatus;
-
 	@Column(name = "withdraw_date", columnDefinition = "DATETIME")
 	private LocalDateTime withdrawDate;
-
+	@Enumerated(EnumType.STRING)
 	@Column(name = "social_provider", columnDefinition = "VARCHAR(20)", nullable = false)
-	private String socialProvider;
-
+	private SocialProviderType socialProvider;
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "job_category_id", columnDefinition = "BIGINT")
 	private JobCategory jobCategory;
 
-	@OneToMany(mappedBy = "member")
-	private List<CoffeeChat> hostChatList = new ArrayList<>();
-
-	@OneToMany(mappedBy = "member")
-	private List<CoffeeChatMember> guestChatList = new ArrayList<>();
-
-	@OneToMany(mappedBy = "member")
-	private List<MemberSkill> memberSkillList = new ArrayList<>();
-
-	@OneToMany(mappedBy = "member")
-	private List<Favorite> favoriteList = new ArrayList<>();
-
 	@Builder
 	public Member(Long id, String email, String nickname, String birth, Gender gender, LocalDateTime joinDate,
 		LocalDateTime lastLoginDate, MemberRole role, MemberAccountStatus accountStatus, LocalDateTime withdrawDate,
-		String socialProvider, JobCategory jobCategory) {
+		SocialProviderType socialProvider, JobCategory jobCategory) {
 		this.id = id;
 		this.email = email;
 		this.nickname = nickname;
@@ -104,5 +90,13 @@ public class Member {
 		this.withdrawDate = withdrawDate;
 		this.socialProvider = socialProvider;
 		this.jobCategory = jobCategory;
+	}
+
+	public boolean isActiveMember() {
+		return this.accountStatus != MemberAccountStatus.WITHDRAW;
+	}
+
+	public boolean isRightSocialProvider(SocialProviderType socialProvider) {
+		return this.socialProvider == socialProvider;
 	}
 }
