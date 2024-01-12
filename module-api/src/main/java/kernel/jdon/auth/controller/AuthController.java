294ffc6555e0deaa0d659539fa91602d37e97963
AN-SOHYEON <hyeon131812@gmail.com>
2024-01-12 18:13:46 +0900

    Feature: oauth-client를 이용한 OAuth2로부터 유저 정보 받아오는 로직 구현 (#106)
    * Feature: Add enum-type of social-provider on Member Entity
    
    * Feature: Add get user email from kakao oauth
    
    * Feature: Add login page
    
    * Chore: Add application.yml properties for oauth2-client setting
    
    * Feature: Get user email from kakao oauth server
    
    * Fix: Move file from member domain to auth domain
    
    * Fix: Move controller from member to auth
    
    * Feature: Add OAuth login using oauth2-client library
    
    * Fix: Add real login in register API
    
    * Fix: Add session timeout setting
    
    * Chore: Delete useless file & dependency
    
    * Fix: Modify ErrorCode to use member domain
    
    * Fix: Modify code to check that email is valid and come from right social provider
    
    * Feature: Add encrpt email&provider info by AES algorithm
    
    * Feature: Add encrpt email&provider info by HMAC algorithm to digital signature
    
    * Fix: Add compare Hmac & decode encryptString for get user email and provider
    
    * Refactor: Modify repository location to fit convention
    
    * Fix: Delete useless comment
    
    * Fix: Use util class & log method
    
    * Fix: Seperate method
    
    * Fix: Modify variable name & delete final keyword
    
    * Feature: Add Errorcode about auth
    
    * Fix: Modify csrf option in security config
    
    * Fix: Add code to save memberskill in DB
    
    * Chore: Delete Qfile

---
+++ module-api/src/main/java/kernel/jdon/auth/controller/AuthController.java
@@ -0,0 +1,41 @@
+package kernel.jdon.auth.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kernel.jdon.auth.dto.request.RegisterRequest;
+import kernel.jdon.auth.dto.response.RegisterResponse;
+import kernel.jdon.auth.dto.response.WithdrawResponse;
+import kernel.jdon.dto.response.CommonResponse;
+import kernel.jdon.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final MemberService memberService;
+
+	@PostMapping("/api/v1/register")
+	public ResponseEntity<CommonResponse> register(@RequestBody RegisterRequest registerRequest) {
+
+		log.info("registerRequest: {}", registerRequest);
+		Long registerMemberId = memberService.register(registerRequest);
+		URI uri = URI.create("/api/v1/member/" + registerMemberId);
+
+		return ResponseEntity.created(uri).body(CommonResponse.of(RegisterResponse.of(registerMemberId)));
+	}
+
+	@DeleteMapping("/api/v1/withdraw")
+	public ResponseEntity<CommonResponse> withdraw() {
+
+		return ResponseEntity.ok(CommonResponse.of(WithdrawResponse.of(1L)));
+	}
+}
