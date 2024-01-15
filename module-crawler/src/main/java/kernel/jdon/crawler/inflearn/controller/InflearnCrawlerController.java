30fa2cf3f8147b4e397aed3697809a5e727e1d99
aacara <budgielover@naver.com>
2024-01-15 17:50:51 +0900

    delete unnecessary exception

--- module-crawler/src/main/java/kernel/jdon/crawler/inflearn/controller/InflearnCrawlerController.java
+++ module-crawler/src/main/java/kernel/jdon/crawler/inflearn/controller/InflearnCrawlerController.java
@@ -1,7 +1,5 @@
 package kernel.jdon.crawler.inflearn.controller;
 
-import java.io.IOException;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +15,7 @@ public class InflearnCrawlerController {
 	private final CrawlerService crawlerService;
 
 	@GetMapping("/api/v1/crawler/inflearn")
-	public ResponseEntity<Void> getInflearnData(@RequestParam(name = "keyword") String keyword) throws IOException {
+	public ResponseEntity<Void> getInflearnData(@RequestParam(name = "keyword") String keyword) {
 		crawlerService.fetchCourseInfo(keyword, 1);
 
 		return ResponseEntity.noContent().build();
