a546a8a535677fd86571deebbfd11bdf96e69791
이윤선 <yoonseon3@gmail.com>
2024-01-17 18:45:51 +0900

    Chore: InflearnJdSkillRepository생성 후 domain 모듈의 InflearnJdSkillDomainRepository 상속 받도록 변경

---
+++ module-domain/src/main/java/kernel/jdon/inflearnjdskill/repository/InflearnJdSkillDomainRepository.java
@@ -0,0 +1,8 @@
+package kernel.jdon.inflearnjdskill.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kernel.jdon.inflearnjdskill.domain.InflearnJdSkill;
+
+public interface InflearnJdSkillDomainRepository extends JpaRepository<InflearnJdSkill, Long> {
+}
