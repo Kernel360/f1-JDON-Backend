282bbf96130cca0a89b538db6026d198a7ab4847
aacara <budgielover@naver.com>
2024-01-22 15:13:53 +0900

    Fix: delete annotation

--- module-api/src/main/java/kernel/jdon/favorite/dto/response/DeleteFavoriteResponse.java
+++ module-api/src/main/java/kernel/jdon/favorite/dto/response/DeleteFavoriteResponse.java
@@ -1,12 +1,9 @@
 package kernel.jdon.favorite.dto.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class DeleteFavoriteResponse {
 	private Long lectureId;
