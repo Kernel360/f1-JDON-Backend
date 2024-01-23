ee1622a4d99076cc839f6cd3fdf78c1c6ecfc9d3
aqrms <aquarimus@gmail.com>
2024-01-23 10:32:57 +0900

    Chore: MemberErrorCode import 업데이트 및 불필요한 임포트 삭제

--- module-api/src/main/java/kernel/jdon/coffeechat/repository/CoffeeChatRepository.java
+++ module-api/src/main/java/kernel/jdon/coffeechat/repository/CoffeeChatRepository.java
@@ -2,8 +2,6 @@ package kernel.jdon.coffeechat.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import kernel.jdon.coffeechat.domain.CoffeeChat;
 
 public interface CoffeeChatRepository extends CoffeeChatDomainRepository {
