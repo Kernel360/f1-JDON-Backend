0b65cc5eef0325dd2ecbc6ecb0ed088a561c6027
anso33 <hyeon131812@gmail.com>
2024-01-24 11:38:56 +0900

    Fix: Add oauthId field in session for withdraw logic

--- module-api/src/main/java/kernel/jdon/auth/dto/SessionUserInfo.java
+++ module-api/src/main/java/kernel/jdon/auth/dto/SessionUserInfo.java
@@ -1,28 +1,33 @@
 package kernel.jdon.auth.dto;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
 import kernel.jdon.member.domain.Member;
 import kernel.jdon.member.domain.MemberRole;
+import kernel.jdon.member.domain.SocialProviderType;
 import lombok.Getter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 public class SessionUserInfo implements Serializable {
 
 	private Long id;
 	private String email;
+	private String oAuthId;
+	private SocialProviderType socialProvider;
 	private LocalDateTime lastLoginDate;
 	private MemberRole role;
 
-	private SessionUserInfo(Member member) {
+	private SessionUserInfo(Member member, UserInfoFromOAuth2 userInfo) {
 		this.id = member.getId();
 		this.email = member.getEmail();
+		this.oAuthId = userInfo.getOAuthId();
+		this.socialProvider = userInfo.getSocialProvider();
 		this.lastLoginDate = member.getLastLoginDate();
 		this.role = member.getRole();
 	}
 
-	public static SessionUserInfo of(Member member) {
-		return new SessionUserInfo(member);
+	public static SessionUserInfo of(Member member, UserInfoFromOAuth2 userInfo) {
+		return new SessionUserInfo(member, userInfo);
 	}
 }
