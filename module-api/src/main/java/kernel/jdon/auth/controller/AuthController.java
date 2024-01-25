f63de245905cd6d12cec05cab9d4f625bec80424
anso33 <hyeon131812@gmail.com>
2024-01-25 21:35:56 +0900

    Feature: Add csrf configuration

--- module-api/src/main/java/kernel/jdon/auth/controller/AuthController.java
+++ module-api/src/main/java/kernel/jdon/auth/controller/AuthController.java
@@ -1,13 +1,5 @@
 package kernel.jdon.auth.controller;
 
-import java.net.URI;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import kernel.jdon.auth.dto.request.RegisterRequest;
 import kernel.jdon.auth.dto.response.RegisterResponse;
 import kernel.jdon.auth.dto.response.WithdrawResponse;
@@ -15,27 +7,38 @@ import kernel.jdon.dto.response.CommonResponse;
 import kernel.jdon.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
-	private final MemberService memberService;
+    private final MemberService memberService;
 
-	@PostMapping("/api/v1/register")
-	public ResponseEntity<CommonResponse> register(@RequestBody RegisterRequest registerRequest) {
+    @PostMapping("/api/v1/register")
+    public ResponseEntity<CommonResponse> register(@RequestBody RegisterRequest registerRequest) {
 
-		log.info("registerRequest: {}", registerRequest);
-		Long registerMemberId = memberService.register(registerRequest);
-		URI uri = URI.create("/api/v1/member/" + registerMemberId);
+        log.info("registerRequest: {}", registerRequest);
+        Long registerMemberId = memberService.register(registerRequest);
+        URI uri = URI.create("/api/v1/member/" + registerMemberId);
 
-		return ResponseEntity.created(uri).body(CommonResponse.of(RegisterResponse.of(registerMemberId)));
-	}
+        return ResponseEntity.created(uri).body(CommonResponse.of(RegisterResponse.of(registerMemberId)));
+    }
 
-	@DeleteMapping("/api/v1/withdraw")
-	public ResponseEntity<CommonResponse> withdraw() {
+    @DeleteMapping("/api/v1/withdraw")
+    public ResponseEntity<CommonResponse> withdraw() {
 
-		return ResponseEntity.ok(CommonResponse.of(WithdrawResponse.of(1L)));
-	}
+        return ResponseEntity.ok(CommonResponse.of(WithdrawResponse.of(1L)));
+    }
+
+    @GetMapping("/api/v1/csrf-token")
+    public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+
+        return ResponseEntity.noContent().header(csrfToken.getHeaderName(), csrfToken.getToken()).build();
+    }
 }
--- module-api/src/main/java/kernel/jdon/config/auth/OAuth2SecurityConfig.java
+++ module-api/src/main/java/kernel/jdon/config/auth/OAuth2SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
@@ -18,12 +21,14 @@ import java.util.List;
 // TODO: 이거하면 왜 HttpSecurity에서 Could not autowire. No beans of 'HttpSecurity' type found. 에러가 사라지는지 모르곘음.
 @Configuration
 public class OAuth2SecurityConfig {
-	private final JdonOAuth2UserService jdonOAuth2UserService;
-	private final JdonOAuth2AuthenticationSuccessHandler jdonOAuth2AuthenticationSuccessHandler;
-	private final JdonAuthExceptionHandler jdonAuthExceptionHandler;
+    private final JdonOAuth2UserService jdonOAuth2UserService;
+    private final JdonOAuth2AuthenticationSuccessHandler jdonOAuth2AuthenticationSuccessHandler;
+    private final JdonAuthExceptionHandler jdonAuthExceptionHandler;
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        XorCsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new XorCsrfTokenRequestAttributeHandler();
+        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName(null);
 
 		http.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
 			CorsConfiguration config = new CorsConfiguration();
@@ -32,27 +37,50 @@ public class OAuth2SecurityConfig {
 			config.setAllowedHeaders(List.of("*"));
 			config.setMaxAge(3600L);
 
-			return config;
-		}));
-		http.exceptionHandling(exceptionConfig -> exceptionConfig
-			.authenticationEntryPoint(jdonAuthExceptionHandler)
-			.accessDeniedHandler(jdonAuthExceptionHandler));
-		http.csrf().disable();
-		http.authorizeHttpRequests(config -> config
-			.requestMatchers("/api/v1/member").hasAnyRole("USER")
-			.requestMatchers("api/**").permitAll()
-			.anyRequest().permitAll());
-		http.oauth2Login(oauth2Configurer -> oauth2Configurer
-			.successHandler(jdonOAuth2AuthenticationSuccessHandler)
-				.failureHandler(jdonAuthExceptionHandler)
-			.userInfoEndpoint(userInfoEndpointConfigurer -> userInfoEndpointConfigurer
-				.userService(jdonOAuth2UserService)));
-		http.logout(logoutConfigurer -> logoutConfigurer
-			.logoutUrl("/api/v1/logout")
-			.logoutSuccessUrl("http://localhost:3000/main") // TODO: 프론트와 상의 후 메인페이지 url로 변경
-			.invalidateHttpSession(true)
-			.deleteCookies("JSESSIONID"));
+            return config;
+        }));
+        http.exceptionHandling(exceptionConfig -> exceptionConfig
+                .authenticationEntryPoint(jdonAuthExceptionHandler)
+                .accessDeniedHandler(jdonAuthExceptionHandler));
+//		http.csrf(csrfConfigurer -> csrfConfigurer.disable());
+        http.csrf(csrfConfigurer -> csrfConfigurer
+                .ignoringRequestMatchers("/api/v1/register", "/api/v1/nickname/duplicate")
+                .csrfTokenRepository(setHttpSessionCsrfTokenRepository())
+                .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+        );
+        http.authorizeHttpRequests(config -> config
+//                .requestMatchers("/api/v1/csrf-token").permitAll()
+                .requestMatchers("/api/v1/**").permitAll()
+//			.requestMatchers("/api/v1/member").hasAnyRole("USER")
+                .anyRequest().permitAll());
+//		http.authorizeHttpRequests(requestConfig -> requestConfig
+//				.requestMatchers("/login").permitAll()
+//				.requestMatchers("/oauth2/authorization/**", "/api/v1/register", "/api/v1/nickname/duplicate").permitAll()
+//				.requestMatchers("/api/v1/member/**", "/api/v1/withdraw", "/api/v1/logout").hasAnyRole("USER")
+//				.requestMatchers("/api/v1/**").permitAll()
+//				.anyRequest().permitAll());
+        http.oauth2Login(oauth2Configurer -> oauth2Configurer
+                .successHandler(jdonOAuth2AuthenticationSuccessHandler)
+                .failureHandler(jdonAuthExceptionHandler)
+                .userInfoEndpoint(userInfoEndpointConfigurer -> userInfoEndpointConfigurer
+                        .userService(jdonOAuth2UserService)));
+        http.logout(logoutConfigurer -> logoutConfigurer
+                .logoutUrl("/api/v1/logout")
+                .logoutSuccessUrl("http://localhost:3000/main") // TODO: 프론트와 상의 후 메인페이지 url로 변경
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID"));
 
-		return http.build();
-	}
+        return http.build();
+    }
+
+    @Bean
+    CsrfTokenRepository setHttpSessionCsrfTokenRepository() {
+        HttpSessionCsrfTokenRepository csrfTokenRepository = new HttpSessionCsrfTokenRepository();
+        log.debug("csrfTokenRepository: {}", csrfTokenRepository);
+        csrfTokenRepository.setHeaderName("X-CSRF-TOKEN");
+        csrfTokenRepository.setParameterName("_csrf");
+        csrfTokenRepository.setSessionAttributeName("CSRF_TOKEN");
+
+        return csrfTokenRepository;
+    }
 }
