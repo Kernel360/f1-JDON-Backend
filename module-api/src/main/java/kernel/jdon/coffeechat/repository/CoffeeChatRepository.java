465a63f53377f28f634126fe44a96d54cba47ed9
aqrms <aquarimus@gmail.com>
2024-01-30 20:34:08 +0900

    Refactor: 커피챗 참여정보 조회시 CoffeeChatMemberRepository 통해서 조회하도록 변경 - CoffeeChatRepository 에 정의한 쿼리 삭제 - CoffeeChatMemberRepository에 쿼리메소드 작성 - 페치조인을 통해 질의 횟수 감소

--- module-api/src/main/java/kernel/jdon/coffeechat/repository/CoffeeChatRepository.java
+++ module-api/src/main/java/kernel/jdon/coffeechat/repository/CoffeeChatRepository.java
@@ -4,8 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import kernel.jdon.coffeechat.domain.CoffeeChat;
 
@@ -15,6 +13,4 @@ public interface CoffeeChatRepository extends CoffeeChatDomainRepository {
 
 	Page<CoffeeChat> findAllByMemberIdAndIsDeletedFalse(Long id, Pageable pageable);
 
-	@Query("SELECT cc FROM CoffeeChat cc JOIN FETCH cc.member m JOIN FETCH m.jobCategory JOIN FETCH cc.coffeeChatMemberList ccm WHERE ccm.member.id = :memberId")
-	Page<CoffeeChat> findAllByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 }
