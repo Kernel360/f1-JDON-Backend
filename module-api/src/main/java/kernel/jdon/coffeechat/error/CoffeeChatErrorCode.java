e850ddb42c55e1696a81fc875a0d181eb9ece8a5
aqrms <aquarimus@gmail.com>
2024-02-01 12:56:46 +0900

    BugFix: 이미 참여한 커피챗 예외에 포함할 에러코드 추가

--- module-api/src/main/java/kernel/jdon/coffeechat/error/CoffeeChatErrorCode.java
+++ module-api/src/main/java/kernel/jdon/coffeechat/error/CoffeeChatErrorCode.java
@@ -12,7 +12,9 @@ public enum CoffeeChatErrorCode implements ErrorCode {
 	INVALID_RECRUIT_COUNT(HttpStatus.BAD_REQUEST, "유효하지 않은 모집인원 입니다."),
 	CAN_NOT_JOIN_OWN_COFFEECHAT(HttpStatus.BAD_REQUEST, "본인이 개설한 커피챗에 참여할 수 없습니다."),
 	EXPIRED_COFFEECHAT(HttpStatus.BAD_REQUEST, "지난 일자의 커피챗은 수정할 수 없습니다."),
-	MEET_DATE_ISBEFORE_NOW(HttpStatus.BAD_REQUEST, "지금보다 이전 시점으로 설정할 수 없습니다.");
+	MEET_DATE_ISBEFORE_NOW(HttpStatus.BAD_REQUEST, "지금보다 이전 시점으로 설정할 수 없습니다."),
+	ALREADY_JOINED_COFFEECHAT(HttpStatus.BAD_REQUEST, "이미 참여한 커피챗 입니다.");
+
 	private final HttpStatus httpStatus;
 	private final String message;
 
