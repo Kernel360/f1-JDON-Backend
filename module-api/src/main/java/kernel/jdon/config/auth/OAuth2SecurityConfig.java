cd5ed4101fc2629a2fcdffede7249cd714dd9e98
anso33 <hyeon131812@gmail.com>
2024-02-02 14:30:50 +0900

    Fix: add allow origin link

--- module-api/src/main/java/kernel/jdon/config/auth/OAuth2SecurityConfig.java
+++ module-api/src/main/java/kernel/jdon/config/auth/OAuth2SecurityConfig.java
@@ -30,7 +30,7 @@ public class OAuth2SecurityConfig {
 		http.cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
 			CorsConfiguration config = new CorsConfiguration();
 			config.setAllowedOrigins(List.of("http://localhost:3000", "https://localhost:3000",
-				"https://peaceful-sopapillas-36c089.netlify.app"));
+				"https://jdon.kr"));
 			config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 			config.setAllowedHeaders(List.of("*"));
 			config.setAllowCredentials(true);
