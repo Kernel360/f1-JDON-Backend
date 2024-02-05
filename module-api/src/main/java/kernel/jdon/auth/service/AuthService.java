2f0bee4f633661832f421870ccea09ab20f104a0
anso33 <hyeon131812@gmail.com>
2024-02-05 18:43:57 +0900

    Chore: 쓸데없는 주석 삭제

--- module-api/src/main/java/kernel/jdon/auth/service/AuthService.java
+++ module-api/src/main/java/kernel/jdon/auth/service/AuthService.java
@@ -9,7 +9,6 @@ import kernel.jdon.member.error.MemberErrorCode;
 import kernel.jdon.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
