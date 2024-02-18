fc8c24d9494c9c1e30f7dac8113110d37dcc2b66
YunNote <zzdd1558@gmail.com>
2024-02-18 23:00:02 +0900

    feature: 에러 코드에 대한 넘버링

--- module-api/src/main/java/kernel/jdon/auth/error/AuthErrorCode.java
+++ module-api/src/main/java/kernel/jdon/auth/error/AuthErrorCode.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
+	//
 	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증되지 않은 사용자입니다."),
 	UNAUTHORIZED_NOT_MATCH_PROVIDER_TYPE(HttpStatus.UNAUTHORIZED, "다른 소셜 로그인으로 가입된 이메일입니다."),
 	UNAUTHORIZED_OAUTH_RETURN_NULL_EMAIL(HttpStatus.UNAUTHORIZED, "소셜 서비스에 이메일을 등록해야 서비스를 이용할 수 있습니다."),
