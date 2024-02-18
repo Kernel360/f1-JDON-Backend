902edfcd3a33c0a55581c44fc5ea8dcb0341b69c
YunNote <zzdd1558@gmail.com>
2024-02-18 23:50:41 +0900

    feature: 미사용

--- module-api/src/main/java/kernel/jdon/inflearncourse/repository/InflearnCourseRepository.java
+++ module-api/src/main/java/kernel/jdon/inflearncourse/repository/InflearnCourseRepository.java
@@ -6,6 +6,8 @@ import kernel.jdon.inflearncourse.domain.InflearnCourse;
 
 public interface InflearnCourseRepository extends InflearnCourseDomainRepository {
 
+
+	// 미사용
 	Optional<InflearnCourse> findByTitle(String title);
 
 	Optional<InflearnCourse> findById(Long id);
