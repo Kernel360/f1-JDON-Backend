d6d7dac6f44e8ccb42b26ab9bc4d34744f3dbd3e
이윤선 <yoonseon3@gmail.com>
2024-02-20 22:01:33 +0900

    Test: SkillServiceImpl.getMemberSkillList 테스트 코드 작성

--- module-api/src/test/java/kernel/jdon/moduleapi/domain/skill/core/SkillServiceImplTest.java
+++ module-api/src/test/java/kernel/jdon/moduleapi/domain/skill/core/SkillServiceImplTest.java
@@ -3,6 +3,7 @@ package kernel.jdon.moduleapi.domain.skill.core;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -12,12 +13,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.io.ClassPathResource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kernel.jdon.jobcategory.domain.JobCategory;
+import kernel.jdon.moduleapi.domain.jobcategory.core.JobCategoryReader;
 
 @DisplayName("Skill Service Impl 테스트")
 @ExtendWith(MockitoExtension.class)
 class SkillServiceImplTest {
 	@Mock
 	private SkillReader skillReader;
+	@Mock
+	private JobCategoryReader jobCategoryReader;
 	@InjectMocks
 	private SkillServiceImpl skillServiceImpl;
 
@@ -53,5 +62,23 @@ class SkillServiceImplTest {
 		// then
 		assertThat(response.getSkillList()).hasSize(3);
 		verify(skillReader, times(1)).findMemberSkillList(memberId);
+	}
+
+	@Test
+	@DisplayName("올바른 직군 ID가 주어졌을 때 getJobCategorySkillList 메서드가 직군별 기술스택에서 '기타' 키워드를 제외한 데이터 개수 만큼 데이터를 응답한다.")
+	void givenValidJobCategoryId_whenFindList_thenReturnCorrectJobCategorySkillList() throws Exception {
+		//given
+		String filePath = "giventest/skill/serviceimpl/givenValidJobCategoryId_whenFindList_thenReturnCorrectJobCategorySkillList.json";
+		ObjectMapper objectMapper = new ObjectMapper();
+		InputStream inputStream = new ClassPathResource(filePath).getInputStream();
+		JobCategory jobCategory = objectMapper.readValue(inputStream, JobCategory.class);
+
+		//when
+		when(jobCategoryReader.findById(jobCategory.getId())).thenReturn(jobCategory);
+		var response = skillServiceImpl.getJobCategorySkillList(jobCategory.getId());
+
+		//then
+		assertThat(response.getSkillList()).hasSize(2);
+		verify(jobCategoryReader, times(1)).findById(jobCategory.getId());
 	}
 }
