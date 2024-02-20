6896178ea0c8b7f37750d12da42bb2426385fc4f
aqrms <aquarimus@gmail.com>
2024-02-20 22:49:28 +0900

    Refactor: infra 계층 구현

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/infrastructure/CoffeeChatReaderImpl.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/infrastructure/CoffeeChatReaderImpl.java
@@ -2,11 +2,22 @@ package kernel.jdon.moduleapi.domain.coffeechat.infrastructure;
 
 import org.springframework.stereotype.Component;
 
+import kernel.jdon.coffeechat.domain.CoffeeChat;
+import kernel.jdon.moduleapi.domain.coffeechat.core.CoffeeChatReader;
+import kernel.jdon.moduleapi.domain.coffeechat.error.CoffeeChatErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CoffeeChatReaderImpl {
+public class CoffeeChatReaderImpl implements CoffeeChatReader {
+
+	private final CoffeeChatRepository coffeeChatRepository;
+
+	@Override
+	public CoffeeChat findExistCoffeeChat(Long coffeeChatId) {
+		return coffeeChatRepository.findByIdAndIsDeletedFalse(coffeeChatId)
+			.orElseThrow(CoffeeChatErrorCode.NOT_FOUND_COFFEECHAT::throwException);
+	}
 }
--- module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/infrastructure/CoffeeChatRepository.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/infrastructure/CoffeeChatRepository.java
@@ -1,6 +1,11 @@
 package kernel.jdon.moduleapi.domain.coffeechat.infrastructure;
 
+import java.util.Optional;
+
+import kernel.jdon.coffeechat.domain.CoffeeChat;
 import kernel.jdon.coffeechat.repository.CoffeeChatDomainRepository;
 
 public interface CoffeeChatRepository extends CoffeeChatDomainRepository {
+
+	Optional<CoffeeChat> findByIdAndIsDeletedFalse(Long id);
 }
