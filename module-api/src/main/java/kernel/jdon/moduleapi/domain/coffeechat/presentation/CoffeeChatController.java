38069ef78c6bda1d1c0c296bffe67ae9f5f811ed
aqrms <aquarimus@gmail.com>
2024-02-20 22:55:26 +0900

    Refactor: presentation 계층 구현 - DtoMapper에서 Info와 Dto는 모든 필드가 동일하기에 unmappedTargetPolicy를 ERROR로 설정했습니다. - InfoMapper에서는 커피챗 엔터티중 Info에 담지 않는 내용이 있어 unmappedSourcePolicy를 WARN으로 낮췄습니다

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/presentation/CoffeeChatController.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/presentation/CoffeeChatController.java
@@ -1,13 +1,30 @@
 package kernel.jdon.moduleapi.domain.coffeechat.presentation;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import kernel.jdon.moduleapi.domain.coffeechat.application.CoffeeChatFacade;
+import kernel.jdon.moduleapi.domain.coffeechat.core.CoffeeChatInfo;
+import kernel.jdon.modulecommon.dto.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class CoffeeChatController {
 
+	private final CoffeeChatFacade coffeeChatFacade;
+	private final CoffeeChatDtoMapper coffeeChatDtoMapper;
+
+	@GetMapping("/api/v1/coffeechats/{id}")
+	public ResponseEntity<CommonResponse<CoffeeChatDto.FindResponse>> getCoffeeChat(
+		@PathVariable(name = "id") Long coffeeChatId) {
+		CoffeeChatInfo.FindResponse info = coffeeChatFacade.getCoffeeChat(coffeeChatId);
+		CoffeeChatDto.FindResponse response = coffeeChatDtoMapper.of(info);
+
+		return ResponseEntity.ok(CommonResponse.of(response));
+	}
 }
