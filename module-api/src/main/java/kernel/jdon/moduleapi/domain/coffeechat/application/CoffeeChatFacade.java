8ab7e1e5143e4d1a3faea3584635f54676abb2bc
aqrms <aquarimus@gmail.com>
2024-02-20 22:50:42 +0900

    Refactor: application 계층 구현 - 응답을 찾아오는 로직과 조회수를 증가시키는 로직을 따로 호출합니다.

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/application/CoffeeChatFacade.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/application/CoffeeChatFacade.java
@@ -2,6 +2,8 @@ package kernel.jdon.moduleapi.domain.coffeechat.application;
 
 import org.springframework.stereotype.Service;
 
+import kernel.jdon.moduleapi.domain.coffeechat.core.CoffeeChatInfo;
+import kernel.jdon.moduleapi.domain.coffeechat.core.CoffeeChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,4 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class CoffeeChatFacade {
+
+	private final CoffeeChatService coffeeChatService;
+
+	public CoffeeChatInfo.FindResponse getCoffeeChat(Long coffeeChatId) {
+		CoffeeChatInfo.FindResponse findResponse = coffeeChatService.getCoffeeChat(coffeeChatId);
+		coffeeChatService.increaseViewCount(coffeeChatId);
+
+		return findResponse;
+	}
+
 }
