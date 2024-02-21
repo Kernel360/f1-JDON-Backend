5dae9308001fb3d287dbae1cbd76edc659ec24c8
aacara <budgielover@naver.com>
2024-02-21 18:50:34 +0900

    Fix: refactor ddd fix error

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/favorite/infrastructure/FavoriteReaderImpl.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/favorite/infrastructure/FavoriteReaderImpl.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import kernel.jdon.favorite.domain.Favorite;
 import kernel.jdon.moduleapi.domain.favorite.application.FavoriteReader;
 import kernel.jdon.moduleapi.domain.favorite.core.FavoriteInfo;
 import kernel.jdon.moduleapi.global.page.CustomPageResponse;
@@ -16,8 +17,22 @@ public class FavoriteReaderImpl implements FavoriteReader {
 
 	@Override
 	public CustomPageResponse<FavoriteInfo.FindResponse> findList(Long memberId, Pageable pageable) {
-		Page<FavoriteInfo.FindResponse> page = favoriteRepository.findFavoriteByMemberId(memberId, pageable);
-		return CustomPageResponse.of(page);
+		Page<Favorite> favoritePage = favoriteRepository.findFavoriteByMemberId(memberId, pageable);
+		Page<FavoriteInfo.FindResponse> infoPage = favoritePage.map(this::convertToFavoriteInfo);
+
+		return CustomPageResponse.of(infoPage);
+	}
+
+	private FavoriteInfo.FindResponse convertToFavoriteInfo(Favorite favorite) {
+		return new FavoriteInfo.FindResponse(
+			favorite.getInflearnCourse().getId(),
+			favorite.getInflearnCourse().getTitle(),
+			favorite.getInflearnCourse().getLectureUrl(),
+			favorite.getInflearnCourse().getImageUrl(),
+			favorite.getInflearnCourse().getInstructor(),
+			favorite.getInflearnCourse().getStudentCount(),
+			favorite.getInflearnCourse().getPrice()
+		);
 	}
 }
 
