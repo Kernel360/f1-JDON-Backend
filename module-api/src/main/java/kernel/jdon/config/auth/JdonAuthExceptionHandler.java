6ef947d2cc1cdf5fc04325f5e9b29a8d1ca28d9d
anso33 <hyeon131812@gmail.com>
2024-02-21 20:56:53 +0900

    Fix: OAuth인증 중 에러 발생시 redirect 하도록 수정

--- module-api/src/main/java/kernel/jdon/config/auth/JdonAuthExceptionHandler.java
+++ module-api/src/main/java/kernel/jdon/config/auth/JdonAuthExceptionHandler.java
@@ -1,8 +1,10 @@
 package kernel.jdon.config.auth;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -11,10 +13,14 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kernel.jdon.auth.error.AuthErrorCode;
+import kernel.jdon.dto.response.ErrorResponse;
+import kernel.jdon.error.ErrorCode;
 import kernel.jdon.global.exception.AuthException;
 
 @Component
@@ -22,9 +28,27 @@ public class JdonAuthExceptionHandler
 	implements AuthenticationEntryPoint, AccessDeniedHandler, AuthenticationFailureHandler {
 
 	private final HandlerExceptionResolver resolver;
+	private final LoginRedirectUrlConfig loginRedirectUrlConfig;
 
-	public JdonAuthExceptionHandler(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+	public JdonAuthExceptionHandler(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver,
+		LoginRedirectUrlConfig config) {
 		this.resolver = resolver;
+		this.loginRedirectUrlConfig = config;
+	}
+
+	private void throwAuthException(HttpServletRequest request, HttpServletResponse response,
+		ErrorCode authErrorCode) throws
+		IOException {
+		AuthException e = new AuthException(authErrorCode);
+		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode(), request);
+
+		String exceptionResponseJson = new ObjectMapper().writeValueAsString(errorResponse);
+		response.setStatus(authErrorCode.getHttpStatus().value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding("utf-8");
+		PrintWriter write = response.getWriter();
+		write.write(exceptionResponseJson);
+		write.flush();
 	}
 
 	@Override
@@ -42,9 +66,11 @@ public class JdonAuthExceptionHandler
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 		AuthenticationException exception) throws IOException, ServletException {
-		if (exception instanceof AuthException) {
-			resolver.resolveException(request, response, null,
-				new AuthException(((AuthException)exception).getErrorCode()));
+		if (AuthErrorCode.UNAUTHORIZED_OAUTH_RETURN_NULL_EMAIL == ((AuthException)exception).getErrorCode()) {
+			response.sendRedirect(loginRedirectUrlConfig.getFailureNotFoundEmail());
+		}
+		if (AuthErrorCode.UNAUTHORIZED_NOT_MATCH_PROVIDER_TYPE == ((AuthException)exception).getErrorCode()) {
+			response.sendRedirect(loginRedirectUrlConfig.getFailureNotMatchProvider());
 		}
 	}
 }
--- module-api/src/main/java/kernel/jdon/config/auth/LoginRedirectUrlConfig.java
+++ module-api/src/main/java/kernel/jdon/config/auth/LoginRedirectUrlConfig.java
@@ -10,11 +10,35 @@ import lombok.RequiredArgsConstructor;
 @ConfigurationProperties(prefix = "redirect-url.login")
 public class LoginRedirectUrlConfig {
 	private final Success success;
+	private final Failure failure;
+
+	public String getSuccessMember() {
+		return this.success.getMember();
+	}
+
+	public String getSuccessGuest() {
+		return this.success.getGuest();
+	}
+
+	public String getFailureNotFoundEmail() {
+		return this.failure.getNotFoundEmail();
+	}
+
+	public String getFailureNotMatchProvider() {
+		return this.failure.getNotMatchProvider();
+	}
 
 	@Getter
 	@RequiredArgsConstructor
 	public static class Success {
 		private final String member;
 		private final String guest;
+	}
+
+	@Getter
+	@RequiredArgsConstructor
+	public static class Failure {
+		private final String notFoundEmail;
+		private final String notMatchProvider;
 	}
 }
