0259a222df76f93b951fcb17ac634aed7740f52d
aqrms <aquarimus@gmail.com>
2024-02-21 19:17:08 +0900

    Test: 커피챗 파사드 상세조회 성공로직 테스트 작성

---
+++ module-api/src/test/java/kernel/jdon/moduleapi/domain/coffeechat/application/CoffeeChatFacadeTest.java
@@ -0,0 +1,41 @@
+package kernel.jdon.moduleapi.domain.coffeechat.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kernel.jdon.moduleapi.domain.coffeechat.core.CoffeeChatInfo;
+import kernel.jdon.moduleapi.domain.coffeechat.core.CoffeeChatService;
+
+@ExtendWith(MockitoExtension.class)
+class CoffeeChatFacadeTest {
+
+	@Mock
+	private CoffeeChatService coffeeChatService;
+	@InjectMocks
+	private CoffeeChatFacade coffeeChatFacade;
+
+	@Test
+	@DisplayName("커피챗 조회 성공 시, 해당 커피챗 조회수를 증가시키고, 올바른 응답을 반환한다")
+	void whenGetCoffeeChat_thenIncreaseViewCount_thenReturnCorrectResponse() {
+		//given
+		CoffeeChatInfo.FindResponse responseMock = mock(CoffeeChatInfo.FindResponse.class);
+		when(coffeeChatService.getCoffeeChat(any(Long.class))).thenReturn(responseMock);
+
+		//when
+		CoffeeChatInfo.FindResponse response = coffeeChatFacade.getCoffeeChat(any(Long.class));
+
+		//then
+		assertThat(response).isEqualTo(responseMock);
+
+		//verify
+		verify(coffeeChatService, times(1)).getCoffeeChat(any(Long.class));
+		verify(coffeeChatService, times(1)).increaseViewCount(any(Long.class));
+	}
+}
