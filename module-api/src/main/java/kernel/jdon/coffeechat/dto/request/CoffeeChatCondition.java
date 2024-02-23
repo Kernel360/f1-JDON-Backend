a114d1de6319299071a0fcdcbd4d6b7d1a32fd44
이윤선 <yoonseon3@gmail.com>
2024-02-23 19:13:13 +0900

    Chore: 미사용 CoffeeChat API 제거

--- module-api/src/main/java/kernel/jdon/coffeechat/dto/request/CoffeeChatCondition.java
+++
@@ -1,9 +0,0 @@
-package kernel.jdon.coffeechat.dto.request;
-
-// @Getter
-// @AllArgsConstructor
-// public class CoffeeChatCondition {
-// 	private CoffeeChatSortCondition sort;
-// 	private String keyword;
-// 	private Long jobCategory;
-// }
--- module-api/src/main/java/kernel/jdon/coffeechat/repository/CustomCoffeeChatRepository.java
+++
@@ -1,5 +0,0 @@
-package kernel.jdon.coffeechat.repository;
-
-public interface CustomCoffeeChatRepository {
-	// Page<FindCoffeeChatListResponse> findCoffeeChatList(Pageable pageable, CoffeeChatCondition coffeeChatCondition);
-}
