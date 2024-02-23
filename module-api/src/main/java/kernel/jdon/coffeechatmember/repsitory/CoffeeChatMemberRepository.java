78e099752b9aee75fc6053eadb363e99174b2230
aqrms <aquarimus@gmail.com>
2024-02-23 10:27:50 +0900

    Refactor: 빈 중복등록 제외를 위한 레거시 레포지토리 표기

--- module-api/src/main/java/kernel/jdon/coffeechatmember/repsitory/CoffeeChatMemberRepository.java
+++ module-api/src/main/java/kernel/jdon/coffeechatmember/repsitory/CoffeeChatMemberRepository.java
@@ -3,15 +3,17 @@ package kernel.jdon.coffeechatmember.repsitory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import kernel.jdon.coffeechatmember.domain.CoffeeChatMember;
 import kernel.jdon.coffeechatmember.repository.CoffeeChatMemberDomainRepository;
 
+@Repository("legacyCoffeeChatMemberRepository")
 public interface CoffeeChatMemberRepository extends CoffeeChatMemberDomainRepository {
 
-	@Query("select ccm from CoffeeChatMember ccm join fetch ccm.coffeeChat cc join fetch cc.member m join fetch m.jobCategory where ccm.member.id = :memberId")
-	Page<CoffeeChatMember> findAllByMemberId(Long memberId, Pageable pageable);
+    @Query("select ccm from CoffeeChatMember ccm join fetch ccm.coffeeChat cc join fetch cc.member m join fetch m.jobCategory where ccm.member.id = :memberId")
+    Page<CoffeeChatMember> findAllByMemberId(Long memberId, Pageable pageable);
 
-	boolean existsByCoffeeChatIdAndMemberId(Long coffeeChatId, Long memberId);
+    boolean existsByCoffeeChatIdAndMemberId(Long coffeeChatId, Long memberId);
 
 }
