c105ef6cd73a70a782c27542c763983ee39a3a0b
aacara <budgielover@naver.com>
2024-02-23 11:24:41 +0900

    Refactor:HttpStatus NOT_FOUND -> CONFLICT

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/favorite/error/FavoriteErrorCode.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/favorite/error/FavoriteErrorCode.java
@@ -10,7 +10,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum FavoriteErrorCode implements ErrorCode, BaseThrowException<FavoriteErrorCode.FavoriteBaseException> {
 	NOT_FOUND_FAVORITE(HttpStatus.NOT_FOUND, "존재하지 않는 찜 정보입니다."),
-	LECTURE_NOT_FAVORITED(HttpStatus.BAD_REQUEST, "찜하지 않은 강의는 찜 취소할 수 없습니다.");
+	LECTURE_NOT_FAVORITED(HttpStatus.CONFLICT, "찜하지 않은 강의는 찜 취소할 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String message;
