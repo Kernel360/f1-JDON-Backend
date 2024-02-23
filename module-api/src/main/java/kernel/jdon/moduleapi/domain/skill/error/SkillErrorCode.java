78d267f9b0a88fab8cf25b8a2dce080b85f103be
anso33 <hyeon131812@gmail.com>
2024-02-23 22:24:40 +0900

    chore: 사용하지 않는 파일 정리 & import 정리

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/skill/error/SkillErrorCode.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/skill/error/SkillErrorCode.java
@@ -2,11 +2,13 @@ package kernel.jdon.moduleapi.domain.skill.error;
 
 import org.springframework.http.HttpStatus;
 
+import kernel.jdon.moduleapi.global.exception.ApiException;
+import kernel.jdon.moduleapi.global.exception.BaseThrowException;
 import kernel.jdon.modulecommon.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public enum SkillErrorCode implements ErrorCode {
+public enum SkillErrorCode implements ErrorCode, BaseThrowException<SkillErrorCode.SkillBaseException> {
 	NOT_FOUND_SKILL(HttpStatus.NOT_FOUND, "존재하지 않는 기술 스킬입니다.");
 
 	private final HttpStatus httpStatus;
@@ -20,5 +22,16 @@ public enum SkillErrorCode implements ErrorCode {
 	@Override
 	public String getMessage() {
 		return message;
+	}
+
+	@Override
+	public SkillErrorCode.SkillBaseException throwException() {
+		return new SkillBaseException(this);
+	}
+
+	public class SkillBaseException extends ApiException {
+		public SkillBaseException(SkillErrorCode skillErrorCode) {
+			super(skillErrorCode);
+		}
 	}
 }
