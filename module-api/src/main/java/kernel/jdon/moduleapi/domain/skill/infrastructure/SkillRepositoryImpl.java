74785701e2ee0248dfeb3a22d6eb9ed373592ad2
이윤선 <yoonseon3@gmail.com>
2024-02-26 16:39:05 +0900

    Fix: SkillRepositoryImpl 클래스명 재설정

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/skill/infrastructure/SkillRepositoryImpl.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/skill/infrastructure/SkillRepositoryImpl.java
@@ -19,7 +19,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class ImplSkillRepository implements CustomSkillRepository {
+public class SkillRepositoryImpl implements CustomSkillRepository {
 
 	private final JPAQueryFactory jpaQueryFactory;
 
