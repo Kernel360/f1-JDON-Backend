92eac2ed8b04b25597cae3415fc87e08f9332197
anso33 <hyeon131812@gmail.com>
2024-02-27 19:32:35 +0900

    fix: Add dto validation

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/auth/presentation/AuthDto.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/auth/presentation/AuthDto.java
@@ -2,6 +2,10 @@ package kernel.jdon.moduleapi.domain.auth.presentation;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import kernel.jdon.moduleapi.global.annotation.validate.Gender;
+import kernel.jdon.moduleapi.global.annotation.validate.IsPastDate;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,11 +17,18 @@ public class AuthDto {
 	@Getter
 	@Builder
 	public static class RegisterRequest {
+		@NotBlank(message = "암호화 문자열이 필요합니다.")
 		private String encrypted;
+		@NotBlank(message = "hmac 값이 필요합니다.")
 		private String hmac;
+		@NotBlank(message = "닉네임은 필수 입력값입니다.")
 		private String nickname;
+		@IsPastDate
 		private String birth;
+		@NotBlank(message = "성별은 필수 입력값입니다.")
+		@Gender
 		private String gender;
+		@Min(1)
 		private Long jobCategoryId;
 		private List<Long> skillList;
 	}
