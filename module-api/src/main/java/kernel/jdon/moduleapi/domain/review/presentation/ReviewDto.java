e2d09bfd1b37c215112ba45e7d276d30312863fd
이윤선 <yoonseon3@gmail.com>
2024-02-27 14:21:18 +0900

    Fix: validation 어노테이션 변경

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/review/presentation/ReviewDto.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/review/presentation/ReviewDto.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import kernel.jdon.moduleapi.global.page.CustomPageInfo;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -18,7 +19,7 @@ public class ReviewDto {
 	@Getter
 	@Builder
 	public static class CreateReviewRequest {
-		@NotBlank(message = "jd 식별자가 없습니다.")
+		@NotNull(message = "jd 식별자가 없습니다.")
 		private final Long jdId;
 		@NotBlank(message = "내용이 없습니다.")
 		private final String content;
