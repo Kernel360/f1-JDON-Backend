1fb08b11eab08886dfd32ee6afc1d9b5b3955094
이윤선 <yoonseon3@gmail.com>
2024-02-29 20:05:43 +0900

    Refactor: 커피챗 도메인 DTO에 final 키워드 추가 및 커피챗 목록 조회 API 응답 viewCount 추가 (#377)
    * Fix: sessionUserInfo -> member 변수명 변경
    
    * Fix: final 키워드 추가
    
    * Fix: CoffeeChat 도메인 final 키워드 추가
    
    * Fix: 커피챗 목록 조회 viewCount 항목 추가

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/core/CoffeeChatInfo.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/core/CoffeeChatInfo.java
@@ -3,7 +3,6 @@ package kernel.jdon.moduleapi.domain.coffeechat.core;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import kernel.jdon.moduleapi.domain.coffeechat.infrastructure.CoffeeChatReaderInfo;
@@ -16,72 +15,69 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CoffeeChatInfo {
 
-    @Getter
-    @Builder
-    public static class FindCoffeeChatResponse {
-        private Long coffeeChatId;
-        private Long hostId;
-        private boolean isParticipant;
-        private String nickname;
-        private String hostJobCategoryName;
-        private String title;
-        private String content;
-        private Long viewCount;
-        private String status;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime meetDate;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime createdDate;
-        private String openChatUrl;
-        private Long totalRecruitCount;
-        private Long currentRecruitCount;
+	@Getter
+	@Builder
+	public static class FindCoffeeChatResponse {
+		private final Long coffeeChatId;
+		private final Long hostId;
+		private final boolean isParticipant;
+		private final String nickname;
+		private final String hostJobCategoryName;
+		private final String title;
+		private final String content;
+		private final Long viewCount;
+		private final String status;
+		private final LocalDateTime meetDate;
+		private final LocalDateTime createdDate;
+		private final String openChatUrl;
+		private final Long totalRecruitCount;
+		private final Long currentRecruitCount;
 
-        public boolean getIsParticipant() {
-            return isParticipant;
-        }
-    }
+		public boolean getIsParticipant() {
+			return isParticipant;
+		}
+	}
 
-    @Getter
-    public static class FindCoffeeChatListResponse {
-        private List<FindCoffeeChat> content;
-        private CustomPageInfo pageInfo;
+	@Getter
+	public static class FindCoffeeChatListResponse {
+		private List<FindCoffeeChat> content;
+		private CustomPageInfo pageInfo;
 
-        public FindCoffeeChatListResponse(List<FindCoffeeChat> content, CustomPageInfo pageInfo) {
-            this.content = content;
-            this.pageInfo = pageInfo;
-        }
-    }
+		public FindCoffeeChatListResponse(List<FindCoffeeChat> content, CustomPageInfo pageInfo) {
+			this.content = content;
+			this.pageInfo = pageInfo;
+		}
+	}
 
-    @Getter
-    @Builder
-    public static class FindCoffeeChat {
-        private Long coffeeChatId;
-        private String nickname;
-        private String hostJobCategoryName;
-        private String title;
-        private String activeStatus;
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        private Boolean isDeleted;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime meetDate;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime createdDate;
-        private Long totalRecruitCount;
-        private Long currentRecruitCount;
+	@Getter
+	@Builder
+	public static class FindCoffeeChat {
+		private final Long coffeeChatId;
+		private final String nickname;
+		private final String hostJobCategoryName;
+		private final String title;
+		private final String activeStatus;
+		@JsonInclude(JsonInclude.Include.NON_NULL)
+		private final Boolean isDeleted;
+		private final LocalDateTime meetDate;
+		private final LocalDateTime createdDate;
+		private final Long totalRecruitCount;
+		private final Long currentRecruitCount;
+		private final Long viewCount;
 
-        public static FindCoffeeChat of(CoffeeChatReaderInfo.FindCoffeeChatListResponse readerInfo) {
-            return FindCoffeeChat.builder()
-                .coffeeChatId(readerInfo.getCoffeeChatId())
-                .nickname(readerInfo.getNickname())
-                .hostJobCategoryName(readerInfo.getHostJobCategoryName())
-                .title(readerInfo.getTitle())
-                .activeStatus(readerInfo.getActiveStatus())
-                .isDeleted(readerInfo.getIsDeleted())
-                .meetDate(readerInfo.getMeetDate())
-                .createdDate(readerInfo.getCreatedDate())
-                .totalRecruitCount(readerInfo.getTotalRecruitCount())
-                .currentRecruitCount(readerInfo.getCurrentRecruitCount())
-                .build();
-        }
-    }
+		public static FindCoffeeChat of(CoffeeChatReaderInfo.FindCoffeeChatListResponse readerInfo) {
+			return FindCoffeeChat.builder()
+				.coffeeChatId(readerInfo.getCoffeeChatId())
+				.nickname(readerInfo.getNickname())
+				.hostJobCategoryName(readerInfo.getHostJobCategoryName())
+				.title(readerInfo.getTitle())
+				.activeStatus(readerInfo.getActiveStatus())
+				.meetDate(readerInfo.getMeetDate())
+				.createdDate(readerInfo.getCreatedDate())
+				.totalRecruitCount(readerInfo.getTotalRecruitCount())
+				.currentRecruitCount(readerInfo.getCurrentRecruitCount())
+				.viewCount(readerInfo.getViewCount())
+				.build();
+		}
+	}
 }
--- module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/presentation/CoffeeChatDto.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/coffeechat/presentation/CoffeeChatDto.java
@@ -15,79 +15,80 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CoffeeChatDto {
-    @Getter
-    @Builder
-    public static class FindCoffeeChatResponse {
-        private Long coffeeChatId;
-        private Long hostId;
-        private boolean isParticipant;
-        private String nickname;
-        private String hostJobCategoryName;
-        private String title;
-        private String content;
-        private Long viewCount;
-        private String status;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime meetDate;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime createdDate;
-        private String openChatUrl;
-        private Long totalRecruitCount;
-        private Long currentRecruitCount;
+	@Getter
+	@Builder
+	public static class FindCoffeeChatResponse {
+		private final Long coffeeChatId;
+		private final Long hostId;
+		private final boolean isParticipant;
+		private final String nickname;
+		private final String hostJobCategoryName;
+		private final String title;
+		private final String content;
+		private final Long viewCount;
+		private final String status;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+		private final LocalDateTime meetDate;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+		private final LocalDateTime createdDate;
+		private final String openChatUrl;
+		private final Long totalRecruitCount;
+		private final Long currentRecruitCount;
 
-        public boolean getIsParticipant() {
-            return isParticipant;
-        }
-    }
+		public boolean getIsParticipant() {
+			return isParticipant;
+		}
+	}
 
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class CreateCoffeeChatRequest {
-        private String title;
-        private String content;
-        private Long totalRecruitCount;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime meetDate;
-        private String openChatUrl;
-    }
+	@Getter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class CreateCoffeeChatRequest {
+		private String title;
+		private String content;
+		private Long totalRecruitCount;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+		private LocalDateTime meetDate;
+		private String openChatUrl;
+	}
 
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class UpdateCoffeeChatRequest {
-        private String title;
-        private String content;
-        private Long totalRecruitCount;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime meetDate;
-        private String openChatUrl;
-    }
+	@Getter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class UpdateCoffeeChatRequest {
+		private String title;
+		private String content;
+		private Long totalRecruitCount;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+		private LocalDateTime meetDate;
+		private String openChatUrl;
+	}
 
-    @Getter
-    @Builder
-    public static class FindCoffeeChatListResponse {
-        private List<FindCoffeeChat> content;
-        private CustomPageInfo pageInfo;
-    }
+	@Getter
+	@Builder
+	public static class FindCoffeeChatListResponse {
+		private final List<FindCoffeeChat> content;
+		private final CustomPageInfo pageInfo;
+	}
 
-    @Getter
-    @Builder
-    public static class FindCoffeeChat {
-        private Long coffeeChatId;
-        private String nickname;
-        private String hostJobCategoryName;
-        private String title;
-        private String activeStatus;
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        private Boolean isDeleted;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime meetDate;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-        private LocalDateTime createdDate;
-        private Long totalRecruitCount;
-        private Long currentRecruitCount;
-    }
+	@Getter
+	@Builder
+	public static class FindCoffeeChat {
+		private final Long coffeeChatId;
+		private final String nickname;
+		private final String hostJobCategoryName;
+		private final String title;
+		private final String activeStatus;
+		@JsonInclude(JsonInclude.Include.NON_NULL)
+		private final Boolean isDeleted;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+		private final LocalDateTime meetDate;
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+		private final LocalDateTime createdDate;
+		private final Long totalRecruitCount;
+		private final Long currentRecruitCount;
+		private final Long viewCount;
+	}
 }
