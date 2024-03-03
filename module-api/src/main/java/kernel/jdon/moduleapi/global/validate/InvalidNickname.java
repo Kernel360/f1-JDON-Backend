457a69543ae05a96d47c0ca499625ba5a7ea6f19
anso33 <hyeon131812@gmail.com>
2024-03-03 16:36:56 +0900

    fix: InvalidNickname enum에서 list로 변환

--- module-api/src/main/java/kernel/jdon/moduleapi/global/validate/InvalidNickname.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/global/validate/InvalidNickname.java
@@ -1,29 +1,20 @@
 package kernel.jdon.moduleapi.global.validate;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public enum InvalidNickname {
-	ADMIN("admin"),
-	ADMINISTER("administer"),
-	어드민("어드민"),
-	관리자("관리자");
-
-	private final String keyword;
+public class InvalidNickname {
+	private static List<String> invalidNicknames = List.of("admin", "administer", "어드민", "관리자");
 
 	public static boolean isInvalid(String keyword) {
-		InvalidNickname findInvalidKeyword = Arrays.stream(InvalidNickname.values())
-			.filter(e -> keyword.toUpperCase().contains(e.getKeyword().toUpperCase()))
+		String findInvalidKeyword = invalidNicknames.stream()
+			.filter(e -> keyword.toUpperCase().contains(e.toUpperCase()))
 			.findAny()
 			.orElse(null);
 
 		return Objects.isNull(findInvalidKeyword);
-	}
-
-	public String getKeyword() {
-		return keyword;
 	}
 }
