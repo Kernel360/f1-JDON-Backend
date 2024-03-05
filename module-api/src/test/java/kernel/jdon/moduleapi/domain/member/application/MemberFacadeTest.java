1a4a7fba94f8ba5fc0fdb5d0691766f1cfec0215
anso33 <hyeon131812@gmail.com>
2024-03-05 18:27:55 +0900

    test: Member facade의 withdraw 메서드 test

--- module-api/src/test/java/kernel/jdon/moduleapi/domain/member/application/MemberFacadeTest.java
+++ module-api/src/test/java/kernel/jdon/moduleapi/domain/member/application/MemberFacadeTest.java
@@ -1,6 +1,7 @@
 package kernel.jdon.moduleapi.domain.member.application;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,16 +10,24 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
+import kernel.jdon.moduleapi.domain.auth.core.CustomOAuth2UserService;
+import kernel.jdon.moduleapi.domain.auth.error.AuthErrorCode;
 import kernel.jdon.moduleapi.domain.member.core.MemberCommand;
 import kernel.jdon.moduleapi.domain.member.core.MemberInfo;
 import kernel.jdon.moduleapi.domain.member.core.MemberService;
+import kernel.jdon.moduleapi.global.exception.ApiException;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Member Facade 테스트")
 class MemberFacadeTest {
 	@Mock
 	private MemberService memberService;
+
+	@Mock
+	private CustomOAuth2UserService customOAuth2UserService;
+
 	@InjectMocks
 	private MemberFacade memberFacade;
 
@@ -90,5 +99,54 @@ class MemberFacadeTest {
 
 		//verify
 		verify(memberService, times(1)).register(mockCommand);
+	}
+
+	@Test
+	@DisplayName("5: 사용자 탈퇴 정보가 주어졌을 때, withdraw 메서드가 탈퇴 성공하면 탈퇴한 memberId를 응답으로 반환한다.")
+	void givenWithdrawInfo_whenWithdraw_thenReturnWithdrawMemberId() {
+		//given
+		final var mockWithdrawCommand = mockWithdrawCommand();
+		final var mockWithdrawResponse = MemberInfo.WithdrawResponse.of(mockWithdrawCommand.getId());
+
+		//when
+		when(customOAuth2UserService.sendDeleteRequestToOAuth2(mockWithdrawCommand)).thenReturn(true);
+		when(memberService.removeMember(mockWithdrawCommand)).thenReturn(mockWithdrawResponse);
+		final var response = memberFacade.withdrawMember(mockWithdrawCommand);
+
+		//then
+		assertThat(response).isEqualTo(mockWithdrawResponse);
+		assertThat(response.getMemberId()).isEqualTo(mockWithdrawResponse.getMemberId());
+
+		//verify
+		verify(customOAuth2UserService, times(1)).sendDeleteRequestToOAuth2(mockWithdrawCommand);
+		verify(memberService, times(1)).removeMember(mockWithdrawCommand);
+	}
+
+	@Test
+	@DisplayName("6: 사용자 탈퇴 정보가 주어졌을 때, withdraw 메서드가 탈퇴 실패하면 500 에러를 던진다.")
+	void givenWithdrawInfo_whenWithdraw_thenThrowException() {
+		//given
+		final var mockWithdrawCommand = mockWithdrawCommand();
+
+		//when
+		when(customOAuth2UserService.sendDeleteRequestToOAuth2(mockWithdrawCommand)).thenReturn(false);
+		final var thrownException = assertThrows(ApiException.class,
+			() -> memberFacade.withdrawMember(mockWithdrawCommand));
+
+		// then
+		assertThat(thrownException.getErrorCode().getHttpStatus().value())
+			.isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		assertThat(thrownException.getErrorCode().getMessage())
+			.isEqualTo(AuthErrorCode.ERROR_FAIL_TO_UNLINK_OAUTH2.getMessage());
+
+		//verify
+		verify(customOAuth2UserService, times(1)).sendDeleteRequestToOAuth2(mockWithdrawCommand);
+		verify(memberService, times(0)).removeMember(mockWithdrawCommand);
+	}
+
+	private MemberCommand.WithdrawRequest mockWithdrawCommand() {
+		return MemberCommand.WithdrawRequest.builder()
+			.id(1L)
+			.build();
 	}
 }
