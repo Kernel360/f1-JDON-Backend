d512789747a04b7083903ea0e11064bc3516a91e
이윤선 <yoonseon3@gmail.com>
2024-03-05 18:15:41 +0900

    Test: ReviewFactoryImpl 단위 테스트 코드 작성

---
+++ module-api/src/test/java/kernel/jdon/moduleapi/domain/review/infrastructure/ReviewFactoryImplTest.java
@@ -0,0 +1,73 @@
+package kernel.jdon.moduleapi.domain.review.infrastructure;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import kernel.jdon.moduleapi.domain.jd.core.JdReader;
+import kernel.jdon.moduleapi.domain.member.core.MemberReader;
+import kernel.jdon.moduleapi.domain.review.core.ReviewCommand;
+import kernel.jdon.moduleapi.domain.review.core.ReviewStore;
+import kernel.jdon.moduledomain.member.domain.Member;
+import kernel.jdon.moduledomain.review.domain.Review;
+import kernel.jdon.moduledomain.wantedjd.domain.WantedJd;
+
+@DisplayName("Review Factory Impl 테스트")
+@ExtendWith(MockitoExtension.class)
+class ReviewFactoryImplTest {
+	@InjectMocks
+	private ReviewFactoryImpl reviewFactoryImpl;
+	@Mock
+	private ReviewStore reviewStore;
+	@Mock
+	private JdReader jdReader;
+	@Mock
+	private MemberReader memberReader;
+
+	@Test
+	@DisplayName("1: 리뷰 등록 정보가 주어지면, saveReview 메서드가 생성된 Review 엔티티를 반환한다.")
+	void givenCreateReviewCommand_whenSaveReview_thenReturnSavedReview() throws Exception {
+		//given
+		final var request = mockCreateCommand();
+		final var mockMember = mock(Member.class);
+		final var mockWantedJd = mock(WantedJd.class);
+		final var mockSavedReview = mockSaveReview(request, mockMember, mockWantedJd);
+
+		//when
+		when(memberReader.findById(request.getMemberId())).thenReturn(mockMember);
+		when(jdReader.findWantedJd(request.getJdId())).thenReturn(mockWantedJd);
+		when(reviewStore.save(any(Review.class))).thenReturn(mockSavedReview);
+		final var response = reviewFactoryImpl.saveReview(request);
+
+		//then
+		assertAll(
+			() -> assertThat(response).isEqualTo(mockSavedReview),
+			() -> assertThat(response.getContent()).isEqualTo(request.getContent())
+		);
+		verify(memberReader, times(1)).findById(request.getMemberId());
+		verify(jdReader, times(1)).findWantedJd(request.getJdId());
+		verify(reviewStore, times(1)).save(any(Review.class));
+	}
+
+	private ReviewCommand.CreateReviewRequest mockCreateCommand() {
+		return ReviewCommand.CreateReviewRequest.builder()
+			.content("content")
+			.build();
+	}
+
+	private Review mockSaveReview(final ReviewCommand.CreateReviewRequest request,
+		final Member mockMember, final WantedJd mockWantedJd) {
+		return Review.builder()
+			.content(request.getContent())
+			.member(mockMember)
+			.wantedJd(mockWantedJd)
+			.build();
+	}
+}
