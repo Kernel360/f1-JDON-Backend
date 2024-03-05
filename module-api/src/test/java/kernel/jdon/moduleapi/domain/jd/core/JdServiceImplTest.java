feb1f31fa36d811da4d79bb5338fbb59bbf40cf8
이윤선 <yoonseon3@gmail.com>
2024-03-05 22:01:30 +0900

    Test: JD Service Impl 테스트 리팩토링 - 검증하는 부분에서 실제로 반환된 객체가 모킹한 객체와 일치하는지 확인하는 코드를 추가했습니다. - 변수명과 테스트 설명을 기존보다 명확하게 변경하였습니다.

--- module-api/src/test/java/kernel/jdon/moduleapi/domain/jd/core/JdServiceImplTest.java
+++ module-api/src/test/java/kernel/jdon/moduleapi/domain/jd/core/JdServiceImplTest.java
@@ -26,45 +26,49 @@ class JdServiceImplTest {
 	private JdInfoMapper jdInfoMapper;
 
 	@Test
-	@DisplayName("1: 올바른 jdId가 주어졌을 때 getJd 메서드가 jd 상세정보와 스킬목록을 반환한다.")
+	@DisplayName("1: jdId가 주어졌을 때 getJd 메서드가 jd 상세정보와 스킬목록을 반환한다.")
 	void givenValidJdId_whenGetJd_thenReturnCorrectJd() throws Exception {
 		//given
 		final var jdId = 1L;
-		final var wantedJd = mock(WantedJd.class);
-		final var skillList = List.of(
+		final var mockWantedJd = mock(WantedJd.class);
+		final var mockSkillList = List.of(
 			mock(JdInfo.FindSkill.class),
 			mock(JdInfo.FindSkill.class));
-		final var findWantedJdResponse = JdInfo.FindWantedJdResponse.builder()
-			.id(jdId)
-			.skillList(skillList)
-			.build();
+		final var mockFindWantedInfo = mockFindWantedInfo(jdId, mockSkillList);
 
 		//when
-		when(jdReader.findWantedJd(jdId)).thenReturn(wantedJd);
-		when(jdReader.findSkillListByWantedJd(wantedJd)).thenReturn(skillList);
-		when(jdInfoMapper.of(wantedJd, skillList)).thenReturn(findWantedJdResponse);
+		when(jdReader.findWantedJd(jdId)).thenReturn(mockWantedJd);
+		when(jdReader.findSkillListByWantedJd(mockWantedJd)).thenReturn(mockSkillList);
+		when(jdInfoMapper.of(mockWantedJd, mockSkillList)).thenReturn(mockFindWantedInfo);
 		final var response = jdServiceImpl.getJd(jdId);
 
 		//then
-		assertThat(response.getSkillList()).isEqualTo(skillList);
+		assertThat(response.getSkillList()).isEqualTo(mockSkillList);
 		verify(jdReader, times(1)).findWantedJd(jdId);
-		verify(jdReader, times(1)).findSkillListByWantedJd(wantedJd);
+		verify(jdReader, times(1)).findSkillListByWantedJd(mockWantedJd);
+	}
+
+	private JdInfo.FindWantedJdResponse mockFindWantedInfo(final Long jdId, final List<JdInfo.FindSkill> skillList) {
+		return JdInfo.FindWantedJdResponse.builder()
+			.id(jdId)
+			.skillList(skillList)
+			.build();
 	}
 
 	@Test
-	@DisplayName("2: 올바른 keyword가 주어졌을 때 getJdList가 jd 목록을 반환한다.")
+	@DisplayName("2: keyword가 주어졌을 때 getJdList가 jd 목록을 반환한다.")
 	void givenValidKeyword_whenGetJdList_thenReturnCorrectJdList() throws Exception {
 		//given
 		final var keyword = "keyword";
 		final var mockPageInfoRequest = mock(PageInfoRequest.class);
-		final var mockFindWantedJdListResponse = mock(JdInfo.FindWantedJdListResponse.class);
+		final var mockFindWantedJdListInfo = mock(JdInfo.FindWantedJdListResponse.class);
 
 		//when
-		when(jdReader.findWantedJdList(mockPageInfoRequest, keyword)).thenReturn(mockFindWantedJdListResponse);
+		when(jdReader.findWantedJdList(mockPageInfoRequest, keyword)).thenReturn(mockFindWantedJdListInfo);
 		final var response = jdServiceImpl.getJdList(mockPageInfoRequest, keyword);
 
 		//then
-		assertThat(response).isNotNull();
+		assertThat(response).isEqualTo(mockFindWantedJdListInfo);
 		verify(jdReader, times(1)).findWantedJdList(mockPageInfoRequest, keyword);
 	}
 }
