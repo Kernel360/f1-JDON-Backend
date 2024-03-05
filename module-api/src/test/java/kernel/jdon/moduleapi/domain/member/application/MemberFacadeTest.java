d14addcfbaaa636436e76d866abd05124cd7ddc2
anso33 <hyeon131812@gmail.com>
2024-03-05 15:11:47 +0900

    test: Member facade의 register, checkNicknameDuplicate 메서드 test

--- module-api/src/test/java/kernel/jdon/moduleapi/domain/member/application/MemberFacadeTest.java
+++ module-api/src/test/java/kernel/jdon/moduleapi/domain/member/application/MemberFacadeTest.java
@@ -58,4 +58,37 @@ class MemberFacadeTest {
 		//verify
 		verify(memberService, times(1)).modifyMember(modifyMemberId, mockModifyMemberCommand);
 	}
+
+	@Test
+	@DisplayName("3: 사용자 아이디와 정보 수정 정보가 주어졌을 때, modify 메서드가 수정을 마친 memberId를 응답으로 반환한다.")
+	void givenNickname_whenCheckNicknameDuplicate() {
+		//given
+		final var mockCommand = mock(MemberCommand.NicknameDuplicateRequest.class);
+
+		//when
+		doNothing().when(memberService).checkNicknameDuplicate(any(MemberCommand.NicknameDuplicateRequest.class));
+		memberFacade.checkNicknameDuplicate(mockCommand);
+
+		//verify
+		verify(memberService, times(1)).checkNicknameDuplicate(any(MemberCommand.NicknameDuplicateRequest.class));
+	}
+
+	@Test
+	@DisplayName("4: 사용자 등록 정보가 주어졌을 때, register 메서드가 등록을 마친 memberId를 응답으로 반환한다.")
+	void givenRegisterInfo_whenRegister_thenReturnMemberId() {
+		//given
+		final var mockCommand = mock(MemberCommand.RegisterRequest.class);
+		final var mockResponse = MemberInfo.RegisterResponse.of(1L);
+
+		//when
+		when(memberService.register(mockCommand)).thenReturn(mockResponse);
+		final var response = memberFacade.register(mockCommand);
+
+		//then
+		assertThat(response).isEqualTo(mockResponse);
+		assertThat(response.getMemberId()).isEqualTo(mockResponse.getMemberId());
+
+		//verify
+		verify(memberService, times(1)).register(mockCommand);
+	}
 }
