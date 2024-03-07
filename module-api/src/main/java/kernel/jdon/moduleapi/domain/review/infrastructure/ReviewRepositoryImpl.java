ab026c4eaaa25edbef374706af4437f5b3629eff
이윤선 <yoonseon3@gmail.com>
2024-03-07 17:58:09 +0900

    Fix: reviewIdIt -> reviewIdLt 메서드명 변경

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/review/infrastructure/ReviewRepositoryImpl.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/review/infrastructure/ReviewRepositoryImpl.java
@@ -16,41 +16,41 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class ReviewRepositoryImpl implements CustomReviewRepository {
-	private final JPAQueryFactory jpaQueryFactory;
+    private final JPAQueryFactory jpaQueryFactory;
 
-	@Override
-	public Slice<ReviewReaderInfo.FindReview> findReviewList(final Long jdId, final Pageable pageable,
-		final Long reviewId) {
-		final List<ReviewReaderInfo.FindReview> content = jpaQueryFactory
-			.select(new QReviewReaderInfo_FindReview(
-				review.id,
-				review.content,
-				member.nickname,
-				review.createdDate,
-				member.id
-			))
-			.from(review)
-			.join(member)
-			.on(review.member.eq(member))
-			.where(
-				reviewIdIt(reviewId),
-				review.wantedJd.id.eq(jdId)
-			)
-			.limit(pageable.getPageSize())
-			.orderBy(review.createdDate.desc())
-			.fetch();
+    @Override
+    public Slice<ReviewReaderInfo.FindReview> findReviewList(final Long jdId, final Pageable pageable,
+        final Long reviewId) {
+        final List<ReviewReaderInfo.FindReview> content = jpaQueryFactory
+            .select(new QReviewReaderInfo_FindReview(
+                review.id,
+                review.content,
+                member.nickname,
+                review.createdDate,
+                member.id
+            ))
+            .from(review)
+            .join(member)
+            .on(review.member.eq(member))
+            .where(
+                reviewIdLt(reviewId),
+                review.wantedJd.id.eq(jdId)
+            )
+            .limit(pageable.getPageSize())
+            .orderBy(review.createdDate.desc())
+            .fetch();
 
-		return new SliceImpl<>(content, pageable, hasNextPage(content, pageable.getPageSize()));
-	}
+        return new SliceImpl<>(content, pageable, hasNextPage(content, pageable.getPageSize()));
+    }
 
-	private boolean hasNextPage(final List<ReviewReaderInfo.FindReview> content, final int pageSize) {
-		return content.size() > pageSize - 1;
-	}
+    private boolean hasNextPage(final List<ReviewReaderInfo.FindReview> content, final int pageSize) {
+        return content.size() > pageSize - 1;
+    }
 
-	private BooleanExpression reviewIdIt(final Long reviewId) {
-		if (reviewId == null) {
-			return null;
-		}
-		return review.id.lt(reviewId);
-	}
+    private BooleanExpression reviewIdLt(final Long reviewId) {
+        if (reviewId == null) {
+            return null;
+        }
+        return review.id.lt(reviewId);
+    }
 }
