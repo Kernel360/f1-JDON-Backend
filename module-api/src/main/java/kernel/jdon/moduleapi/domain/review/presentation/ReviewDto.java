17abe36628c14fc9ff0261ee9d7b6eaf91e1131b
이윤선 <yoonseon3@gmail.com>
2024-03-07 18:02:44 +0900

    Fix: 인텐트 수정

--- module-api/src/main/java/kernel/jdon/moduleapi/domain/review/presentation/ReviewDto.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/review/presentation/ReviewDto.java
@@ -16,39 +16,38 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReviewDto {
+    @Getter
+    @Builder
+    public static class CreateReviewRequest {
+        @NotNull(message = "jd 식별자가 없습니다.")
+        private final Long jdId;
+        @NotBlank(message = "내용은 필수 입력 항목 입니다.")
+        @Size(min = 10, max = 500, message = "내용은 10자 이상 500자 이하로 작성해주세요.")
+        private final String content;
+    }
 
-	@Getter
-	@Builder
-	public static class CreateReviewRequest {
-		@NotNull(message = "jd 식별자가 없습니다.")
-		private final Long jdId;
-		@NotBlank(message = "내용은 필수 입력 항목 입니다.")
-		@Size(min = 10, max = 500, message = "내용은 10자 이상 500자 이하로 작성해주세요.")
-		private final String content;
-	}
+    @Getter
+    @Builder
+    public static class CreateReviewResponse {
+        private final Long reviewId;
+    }
 
-	@Getter
-	@Builder
-	public static class CreateReviewResponse {
-		private final Long reviewId;
-	}
+    @Getter
+    @Builder
+    public static class FindReviewListResponse {
+        private final List<FindReview> content;
+        private final CustomSliceInfo pageInfo;
+    }
 
-	@Getter
-	@Builder
-	public static class FindReviewListResponse {
-		private final List<FindReview> content;
-		private final CustomSliceInfo pageInfo;
-	}
-
-	@Getter
-	@Builder
-	public static class FindReview {
-		private final Long id;
-		private final String content;
-		private final String nickname;
-		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
-		private final LocalDateTime createdDate;
-		private final Long memberId;
-	}
+    @Getter
+    @Builder
+    public static class FindReview {
+        private final Long id;
+        private final String content;
+        private final String nickname;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        private final LocalDateTime createdDate;
+        private final Long memberId;
+    }
 
 }
