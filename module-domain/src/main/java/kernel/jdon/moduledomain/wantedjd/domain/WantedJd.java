04ca886fb98de59f9eb1eaeca6b15d1112293ddb
이윤선 <yoonseon3@gmail.com>
2024-03-08 17:31:45 +0900

    BugFix : scrapingDate 필드 타입 변경 (#437)

--- module-domain/src/main/java/kernel/jdon/moduledomain/wantedjd/domain/WantedJd.java
+++ module-domain/src/main/java/kernel/jdon/moduledomain/wantedjd/domain/WantedJd.java
@@ -1,5 +1,6 @@
 package kernel.jdon.moduledomain.wantedjd.domain;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,69 +34,69 @@ import lombok.NoArgsConstructor;
 @Table(name = "wanted_jd", uniqueConstraints = @UniqueConstraint(columnNames = {"detail_id", "job_category_id"}))
 public class WantedJd {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name = "company_name", columnDefinition = "VARCHAR(50)", nullable = false)
-	private String companyName;
+    @Column(name = "company_name", columnDefinition = "VARCHAR(50)", nullable = false)
+    private String companyName;
 
-	@Column(name = "title", columnDefinition = "VARCHAR(255)", nullable = false)
-	private String title;
+    @Column(name = "title", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String title;
 
-	@Column(name = "detail_id", columnDefinition = "BIGINT", nullable = false)
-	private Long detailId;
+    @Column(name = "detail_id", columnDefinition = "BIGINT", nullable = false)
+    private Long detailId;
 
-	@Column(name = "detail_url", columnDefinition = "VARCHAR(255)", nullable = false)
-	private String detailUrl;
+    @Column(name = "detail_url", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String detailUrl;
 
-	@Column(name = "image_url", columnDefinition = "TEXT", nullable = false)
-	private String imageUrl;
+    @Column(name = "image_url", columnDefinition = "TEXT", nullable = false)
+    private String imageUrl;
 
-	@Column(name = "requirements", columnDefinition = "TEXT")
-	private String requirements;
+    @Column(name = "requirements", columnDefinition = "TEXT")
+    private String requirements;
 
-	@Column(name = "main_tasks", columnDefinition = "TEXT")
-	private String mainTasks;
+    @Column(name = "main_tasks", columnDefinition = "TEXT")
+    private String mainTasks;
 
-	@Column(name = "intro", columnDefinition = "TEXT")
-	private String intro;
+    @Column(name = "intro", columnDefinition = "TEXT")
+    private String intro;
 
-	@Column(name = "benefits", columnDefinition = "TEXT")
-	private String benefits;
+    @Column(name = "benefits", columnDefinition = "TEXT")
+    private String benefits;
 
-	@Column(name = "preferredPoints", columnDefinition = "TEXT")
-	private String preferredPoints;
+    @Column(name = "preferredPoints", columnDefinition = "TEXT")
+    private String preferredPoints;
 
-	@CreatedDate
-	@Column(name = "scraping_date", columnDefinition = "TEXT", nullable = false)
-	private String scrapingDate;
+    @CreatedDate
+    @Column(name = "scraping_date", columnDefinition = "DATETIME", nullable = false)
+    private LocalDateTime scrapingDate;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "job_category_id", columnDefinition = "BIGINT")
-	private JobCategory jobCategory;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "job_category_id", columnDefinition = "BIGINT")
+    private JobCategory jobCategory;
 
-	@OneToMany(mappedBy = "wantedJd")
-	private List<WantedJdSkill> skillList = new ArrayList<>();
+    @OneToMany(mappedBy = "wantedJd")
+    private List<WantedJdSkill> skillList = new ArrayList<>();
 
-	@OneToMany(mappedBy = "wantedJd")
-	private List<Review> reviewList = new ArrayList<>();
+    @OneToMany(mappedBy = "wantedJd")
+    private List<Review> reviewList = new ArrayList<>();
 
-	@Builder
-	public WantedJd(String companyName, String title, Long detailId, String detailUrl, String imageUrl,
-		String requirements, String mainTasks, String intro, String benefits, String preferredPoints,
-		String scrapingDate, JobCategory jobCategory) {
-		this.companyName = companyName;
-		this.title = title;
-		this.detailId = detailId;
-		this.detailUrl = detailUrl;
-		this.imageUrl = imageUrl;
-		this.requirements = requirements;
-		this.mainTasks = mainTasks;
-		this.intro = intro;
-		this.benefits = benefits;
-		this.preferredPoints = preferredPoints;
-		this.scrapingDate = scrapingDate;
-		this.jobCategory = jobCategory;
-	}
+    @Builder
+    public WantedJd(String companyName, String title, Long detailId, String detailUrl, String imageUrl,
+        String requirements, String mainTasks, String intro, String benefits, String preferredPoints,
+        LocalDateTime scrapingDate, JobCategory jobCategory) {
+        this.companyName = companyName;
+        this.title = title;
+        this.detailId = detailId;
+        this.detailUrl = detailUrl;
+        this.imageUrl = imageUrl;
+        this.requirements = requirements;
+        this.mainTasks = mainTasks;
+        this.intro = intro;
+        this.benefits = benefits;
+        this.preferredPoints = preferredPoints;
+        this.scrapingDate = scrapingDate;
+        this.jobCategory = jobCategory;
+    }
 }
