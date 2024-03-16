9501eda918b2020c01c3b0988564e5df7d62f7f8
이윤선 <yoonseon3@gmail.com>
2024-03-16 23:28:04 +0900

    Feat: 직군별 Reader를 통해 스크래핑 하도록 구현

--- module-batch/src/main/java/kernel/jdon/modulebatch/jd/job/AllWantedJobScrapingJobConfig.java
+++ module-batch/src/main/java/kernel/jdon/modulebatch/jd/job/AllWantedJobScrapingJobConfig.java
@@ -12,17 +12,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import kernel.jdon.modulebatch.jd.processor.WantedJdItemProcessor;
-import kernel.jdon.modulebatch.jd.reader.WantedJdItemReader;
+import kernel.jdon.modulebatch.jd.reader.BackendWantedJdItemReader;
+import kernel.jdon.modulebatch.jd.reader.FrontendWantedJdItemReader;
 import kernel.jdon.modulebatch.jd.reader.dto.WantedJobDetailListResponse;
-import kernel.jdon.modulebatch.jd.search.JobSearchJobPosition;
 import kernel.jdon.modulebatch.jd.writer.WantedJdItemWriter;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
-public class WantedJdJobConfig {
+public class AllWantedJobScrapingJobConfig {
+    private static final String JOB_NAME = "정기_원티드_채용공고_스크래핑_";
+    private static final String BEAN_PREFIX = JOB_NAME + "_";
+
     private static final int CHUNK_SIZE = 3;
-    private final WantedJdItemReader wantedJdItemReader;
+
+    private final FrontendWantedJdItemReader frontendWantedJdItemReader;
+    private final BackendWantedJdItemReader backendWantedJdItemReader;
     private final WantedJdItemProcessor wantedJdItemProcessor;
     private final WantedJdItemWriter wantedJdItemWriter;
     private final PlatformTransactionManager platformTransactionManager;
@@ -31,17 +36,17 @@ public class WantedJdJobConfig {
     public Job wantedJdJob(JobRepository jobRepository, Step backendWantedJdStep) {
         return new JobBuilder("wantedJdJob", jobRepository)
             .incrementer(new RunIdIncrementer())
-            .start(backendWantedJdStep(jobRepository)) // 백엔드 스크래핑
-            .next(frontendWantedJdStep(jobRepository)) // 프론트엔드 스크래핑
+            .start(backendWantedJdStep(jobRepository)) // 백엔드 JD 스크래핑
+            .next(frontendWantedJdStep(jobRepository)) // 프론트엔드 JD 스크래핑
             .build();
     }
 
     @Bean
     @JobScope
     public Step backendWantedJdStep(JobRepository jobRepository) {
-        return new StepBuilder("backendWantedJdStep", jobRepository)
+        return new StepBuilder(BEAN_PREFIX + "백엔드_step", jobRepository)
             .<WantedJobDetailListResponse, WantedJobDetailListResponse>chunk(CHUNK_SIZE, platformTransactionManager)
-            .reader(backendWantedJdItemReader())
+            .reader(backendWantedJdItemReader)
             .processor(wantedJdItemProcessor)
             .writer(wantedJdItemWriter)
             .build();
@@ -50,26 +55,12 @@ public class WantedJdJobConfig {
     @Bean
     @JobScope
     public Step frontendWantedJdStep(JobRepository jobRepository) {
-        return new StepBuilder("frontendWantedJdStep", jobRepository)
+        return new StepBuilder(BEAN_PREFIX + "프론트엔드_step", jobRepository)
             .<WantedJobDetailListResponse, WantedJobDetailListResponse>chunk(CHUNK_SIZE, platformTransactionManager)
-            .reader(frontendWantedJdItemReader())
+            .reader(frontendWantedJdItemReader)
             .processor(wantedJdItemProcessor)
             .writer(wantedJdItemWriter)
             .build();
-    }
-
-    @Bean
-    public WantedJdItemReader backendWantedJdItemReader() {
-        JobSearchJobPosition backendJobPosition = JobSearchJobPosition.JOB_POSITION_SERVER;
-        // todo : 여기에 어떻게 JobSearchJobPosition.JOB_POSITION_SERVER를 전달할 수 있을까요?
-        return new WantedJdItemReader(null, null, null);
-    }
-
-    @Bean
-    public WantedJdItemReader frontendWantedJdItemReader() {
-        JobSearchJobPosition frontendJobPosition = JobSearchJobPosition.JOB_POSITION_FRONTEND;
-        // todo : 여기에 어떻게 JobSearchJobPosition.JOB_POSITION_SERVER를 전달할 수 있을까요?
-        return new WantedJdItemReader(null, null, null);
     }
 
 }
---
+++ module-batch/src/main/java/kernel/jdon/modulebatch/jd/reader/FrontendWantedJdItemReader.java
@@ -0,0 +1,46 @@
+package kernel.jdon.modulebatch.jd.reader;
+
+import java.util.List;
+
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.stereotype.Component;
+
+import kernel.jdon.modulebatch.config.ScrapingWantedProperties;
+import kernel.jdon.modulebatch.jd.reader.dto.WantedJobDetailListResponse;
+import kernel.jdon.modulebatch.jd.reader.dto.WantedJobDetailResponse;
+import kernel.jdon.modulebatch.jd.search.JobSearchJobPosition;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@StepScope
+@Component
+@RequiredArgsConstructor
+public class FrontendWantedJdItemReader implements ItemReader<WantedJobDetailListResponse> {
+    private final WantedJdClient wantedJdClient;
+    private final ScrapingWantedProperties scrapingWantedProperties;
+    private int offset = 0;
+
+    @Override
+    public WantedJobDetailListResponse read() throws
+        Exception,
+        UnexpectedInputException,
+        ParseException,
+        NonTransientResourceException {
+        final JobSearchJobPosition jobPosition = JobSearchJobPosition.JOB_POSITION_FRONTEND;
+        final List<WantedJobDetailResponse> jobDetailList = wantedJdClient.getJobDetailList(jobPosition, offset);
+
+        incrementOffset();
+
+        return !jobDetailList.isEmpty() ? new WantedJobDetailListResponse(jobDetailList) : null;
+    }
+
+    private void incrementOffset() {
+        offset += scrapingWantedProperties.getMaxFetchJdListOffset();
+    }
+
+}
