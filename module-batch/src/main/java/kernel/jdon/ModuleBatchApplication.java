03a143d702479156fe37c8ec66155c84a38a0e95
이윤선 <yoonseon3@gmail.com>
2024-03-19 12:05:05 +0900

    Fix: @EnableBatchProcessing 어노테이션 추가

--- module-batch/src/main/java/kernel/jdon/ModuleBatchApplication.java
+++ module-batch/src/main/java/kernel/jdon/ModuleBatchApplication.java
@@ -1,5 +1,6 @@
 package kernel.jdon;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,6 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EntityScan(basePackages = "kernel.jdon")
+@EnableBatchProcessing
 public class ModuleBatchApplication {
 
     public static void main(String[] args) {
