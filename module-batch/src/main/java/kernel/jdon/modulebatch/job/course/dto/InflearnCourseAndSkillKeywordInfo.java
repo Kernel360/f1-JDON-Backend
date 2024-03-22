b0ef4029c978a33a4ec884a911118199d8ef3dc8
aacara <budgielover@naver.com>
2024-03-22 18:06:43 +0900

    Fix: @AllargsConstructor

--- module-batch/src/main/java/kernel/jdon/modulebatch/job/course/dto/InflearnCourseAndSkillKeywordInfo.java
+++ module-batch/src/main/java/kernel/jdon/modulebatch/job/course/dto/InflearnCourseAndSkillKeywordInfo.java
@@ -4,17 +4,14 @@ import java.util.List;
 
 import kernel.jdon.moduledomain.inflearncourse.domain.InflearnCourse;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class InflearnCourseAndSkillKeywordInfo {
     private String skillKeyword;
     private List<InflearnCourse> inflearnCourseList;
-
-    public InflearnCourseAndSkillKeywordInfo(String skillKeyword, List<InflearnCourse> inflearnCourseList) {
-        this.skillKeyword = skillKeyword;
-        this.inflearnCourseList = inflearnCourseList;
-    }
 }
