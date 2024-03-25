abac989cd0af6059db6cfdc9aefc6d15ea4bc909
aacara <budgielover@naver.com>
2024-03-25 15:10:54 +0900

    Chore: 원복

--- module-batch/src/main/java/kernel/jdon/ModuleBatchApplication.java
+++ module-batch/src/main/java/kernel/jdon/ModuleBatchApplication.java
@@ -3,12 +3,12 @@ package kernel.jdon;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableScheduling
+@EntityScan(basePackages = "kernel.jdon")
 @EnableBatchProcessing
 public class ModuleBatchApplication {
 
