80d762638f95a4b77e28b9fa5e455ac6260367c8
LEE-YOONSEON <yoonseon3@gmail.com>
2024-03-25 17:39:26 +0900

    Refactor: 기술스택 이력 및 기술스택 저장 벌크 insert로 변경하여 성능 최적화 및 스케줄러 등록 (#503)

--- module-batch/src/main/java/kernel/jdon/modulebatch/job/jd/writer/WantedJdItemWriter.java
+++ module-batch/src/main/java/kernel/jdon/modulebatch/job/jd/writer/WantedJdItemWriter.java
@@ -1,27 +1,19 @@
 package kernel.jdon.modulebatch.job.jd.writer;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
-import kernel.jdon.modulebatch.domain.skill.error.SkillErrorCode;
-import kernel.jdon.modulebatch.domain.skill.repository.SkillRepository;
-import kernel.jdon.modulebatch.domain.skillhistory.repository.SkillHistoryRepository;
+import kernel.jdon.modulebatch.domain.skillhistory.repository.SkillHistoryDao;
 import kernel.jdon.modulebatch.domain.wantedjd.repository.WantedJdRepository;
-import kernel.jdon.modulebatch.domain.wantedjdskill.repository.WantedJdSkillRepository;
-import kernel.jdon.modulebatch.job.jd.reader.condition.JobSearchJobPosition;
+import kernel.jdon.modulebatch.domain.wantedjdskill.repository.WantedJdSkillDto;
 import kernel.jdon.modulebatch.job.jd.reader.dto.WantedJobDetailListResponse;
 import kernel.jdon.modulebatch.job.jd.reader.dto.WantedJobDetailResponse;
 import kernel.jdon.moduledomain.jobcategory.domain.JobCategory;
-import kernel.jdon.moduledomain.skill.domain.Skill;
-import kernel.jdon.moduledomain.skill.domain.SkillType;
-import kernel.jdon.moduledomain.skillhistory.domain.SkillHistory;
 import kernel.jdon.moduledomain.wantedjd.domain.WantedJd;
-import kernel.jdon.moduledomain.wantedjdskill.domain.WantedJdSkill;
 import lombok.RequiredArgsConstructor;
 
 @StepScope
@@ -29,9 +21,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WantedJdItemWriter implements ItemWriter<WantedJobDetailListResponse> {
     private final WantedJdRepository wantedJdRepository;
-    private final SkillHistoryRepository skillHistoryRepository;
-    private final WantedJdSkillRepository wantedJdSkillRepository;
-    private final SkillRepository skillRepository;
+    private final WantedJdSkillDto wantedJdSkillDto;
+    private final SkillHistoryDao skillHistoryDao;
 
     @Override
     public void write(Chunk<? extends WantedJobDetailListResponse> chunk) throws Exception {
@@ -57,13 +48,13 @@ public class WantedJdItemWriter implements ItemWriter<WantedJobDetailListRespons
 
     private void changeSkillHistory(final WantedJd findWantedJd, final JobCategory jdJobCategory,
         final List<WantedJobDetailResponse.WantedSkill> wantedJdSkillList) {
-        skillHistoryRepository.deleteAllByWantedJdId(findWantedJd.getId());
+        skillHistoryDao.deleteAllByWantedJdId(findWantedJd.getId());
         createSkillHistory(jdJobCategory, findWantedJd, wantedJdSkillList);
     }
 
     private void changeWantedJdSkillList(final WantedJobDetailResponse wantedJobDetail, final WantedJd findWantedJd,
         final List<WantedJobDetailResponse.WantedSkill> wantedJdSkillList) {
-        wantedJdSkillRepository.deleteAllByWantedJdId(findWantedJd.getId());
+        wantedJdSkillDto.deleteAllByWantedJdId(findWantedJd.getId());
         createWantedJdSkill(wantedJobDetail, findWantedJd, wantedJdSkillList);
     }
 
@@ -74,35 +65,14 @@ public class WantedJdItemWriter implements ItemWriter<WantedJobDetailListRespons
     /** 원본 기술스택 명 이력 저장 **/
     private void createSkillHistory(final JobCategory jobCategory, final WantedJd wantedJd,
         final List<WantedJobDetailResponse.WantedSkill> wantedDetailSkillList) {
-
-        for (WantedJobDetailResponse.WantedSkill wantedJdDetailSkill : wantedDetailSkillList) {
-            final SkillHistory skillHistory = new SkillHistory(wantedJdDetailSkill.getKeyword(), jobCategory, wantedJd);
-            skillHistoryRepository.save(skillHistory);
-        }
+        skillHistoryDao.saveSkillHistoryList(jobCategory.getId(), wantedJd.getId(), wantedDetailSkillList);
     }
 
     /** 기술스택 저장 **/
-    private void createWantedJdSkill(WantedJobDetailResponse wantedJobDetail,
-        WantedJd wantedJd, List<WantedJobDetailResponse.WantedSkill> wantedDetailSkillList) {
-        for (WantedJobDetailResponse.WantedSkill wantedSkill : wantedDetailSkillList) {
-            final String skillKeyword = wantedSkill.getKeyword();
-            final JobSearchJobPosition jobPosition = wantedJobDetail.getJobPosition();
-            final JobCategory jobCategory = wantedJobDetail.getJobCategory();
-
-            final Optional<SkillType> findSkillType = jobPosition.getSkillTypeList().stream()
-                .filter(skillType -> skillType.getKeyword().equalsIgnoreCase(skillKeyword))
-                .findFirst();
-
-            final Skill findSkill = findSkillType
-                .map(skillType -> findByJobCategoryIdAndKeyword(jobCategory, skillType.getKeyword()))
-                .orElseGet(() -> findByJobCategoryIdAndKeyword(jobCategory, SkillType.getOrderKeyword()));
-
-            wantedJdSkillRepository.save(new WantedJdSkill(findSkill, wantedJd));
-        }
-    }
-
-    private Skill findByJobCategoryIdAndKeyword(final JobCategory jobCategory, final String matchedSkillType) {
-        return skillRepository.findByJobCategoryIdAndKeyword(jobCategory.getId(), matchedSkillType)
-            .orElseThrow(SkillErrorCode.NOT_FOUND_SKILL::throwException);
+    private void createWantedJdSkill(
+        final WantedJobDetailResponse wantedJobDetail,
+        final WantedJd wantedJd,
+        final List<WantedJobDetailResponse.WantedSkill> wantedDetailSkillList) {
+        wantedJdSkillDto.saveWantedJdSkillList(wantedJobDetail, wantedJd, wantedDetailSkillList);
     }
 }
