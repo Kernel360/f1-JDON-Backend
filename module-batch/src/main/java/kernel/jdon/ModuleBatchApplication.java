24c47c1cf31bae34428cdf8034068dc526e80d26
aacara <budgielover@naver.com>
2024-03-25 15:05:29 +0900

    Fix: 스케줄러 내 이름 변경

--- module-batch/src/main/java/kernel/jdon/ModuleBatchApplication.java
+++ module-batch/src/main/java/kernel/jdon/ModuleBatchApplication.java
@@ -3,12 +3,12 @@ package kernel.jdon;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EntityScan(basePackages = "kernel.jdon")
+@EnableScheduling
 @EnableBatchProcessing
 public class ModuleBatchApplication {
 
