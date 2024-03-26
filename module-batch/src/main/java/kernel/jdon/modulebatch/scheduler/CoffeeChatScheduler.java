c9b1c3ccc0abc10d02c3f1eedba63b5385308bc3
aqrms <aquarimus@gmail.com>
2024-03-26 21:54:55 +0900

    feat: Slack 알림 전송 추가

--- module-batch/src/main/java/kernel/jdon/modulebatch/scheduler/CoffeeChatScheduler.java
+++ module-batch/src/main/java/kernel/jdon/modulebatch/scheduler/CoffeeChatScheduler.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import kernel.jdon.modulebatch.domain.coffeechat.repository.CoffeeChatRepository;
+import kernel.jdon.modulecommon.slack.SlackSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,15 +14,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class CoffeeChatScheduler {
     private final CoffeeChatRepository coffeeChatRepository;
+    private final SlackSender slackSender;
 
     @Transactional
     @Scheduled(cron = "0 30 * * * ?")
     public void expirePastCoffeeChats() {
         log.info("[expirePastCoffeeChats] 스케쥴러 실행");
         try {
+            slackSender.sendSchedulerStart("expirePastCoffeeChats");
             coffeeChatRepository.updateStatusToCloseForPastCoffeeChats();
         } catch (Exception e) {
             log.error("[expirePastCoffeeChats] 스케쥴러 실행 중 Error 발생", e);
         }
+        log.info("[expirePastCoffeeChats] 스케쥴러 종료");
+        slackSender.sendSchedulerEnd("expirePastCoffeeChats");
     }
 }
