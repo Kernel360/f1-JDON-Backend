d93833876e1eba21ed3ca07d7b4106e7e9dc04ba
aqrms <aquarimus@gmail.com>
2024-03-26 12:22:14 +0900

    Setting: 커피챗 도메인 패키지 생성 및 레포지토리 추가

---
+++ module-batch/src/main/java/kernel/jdon/modulebatch/domain/coffeechat/repository/CoffeeChatRepository.java
@@ -0,0 +1,6 @@
+package kernel.jdon.modulebatch.domain.coffeechat.repository;
+
+import kernel.jdon.moduledomain.coffeechat.repository.CoffeeChatDomainRepository;
+
+public interface CoffeeChatRepository extends CoffeeChatDomainRepository {
+}
