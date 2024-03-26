653fd7ac4ec71e5c8aa72d3475bc42426f9c5579
aacara <budgielover@naver.com>
2024-03-26 11:00:19 +0900

    Refactor: 캐시 초기화 부분 분리

---
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/skill/config/SkillCacheInitializer.java
@@ -0,0 +1,42 @@
+package kernel.jdon.moduleapi.domain.skill.config;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import kernel.jdon.moduleapi.domain.skill.infrastructure.SkillRepository;
+import kernel.jdon.moduledomain.skill.domain.Skill;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SkillCacheInitializer {
+
+    private static final String SKILL_KEYWORDS = "SkillKeywords";
+    private final SkillRepository skillRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private HashOperations<String, String, Set<String>> hashOperations;
+
+    @PostConstruct
+    public void initializeCache() {
+        hashOperations = redisTemplate.opsForHash();
+        loadSkillsIntoCache();
+    }
+
+    private void loadSkillsIntoCache() {
+        List<Skill> skillList = skillRepository.findAll();
+        skillList.forEach(skill -> {
+            String keyword = skill.getKeyword().toLowerCase();
+            cacheKeyword(keyword);
+        });
+    }
+
+    private void cacheKeyword(String keyword) {
+        hashOperations.putIfAbsent(SKILL_KEYWORDS, keyword, new HashSet<>(Set.of(keyword)));
+    }
+}
--- module-api/src/main/java/kernel/jdon/moduleapi/domain/skill/infrastructure/keyword/SkillKeywordCache.java
+++ module-api/src/main/java/kernel/jdon/moduleapi/domain/skill/infrastructure/keyword/SkillKeywordCache.java
@@ -1,7 +1,6 @@
 package kernel.jdon.moduleapi.domain.skill.infrastructure.keyword;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -12,8 +11,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
-import kernel.jdon.moduleapi.domain.skill.infrastructure.SkillRepository;
-import kernel.jdon.moduledomain.skill.domain.Skill;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -21,28 +18,13 @@ import lombok.RequiredArgsConstructor;
 public class SkillKeywordCache {
 
     private static final String SKILL_KEYWORDS = "SkillKeywords";
-    private final SkillRepository skillRepository;
     private final SkillKeywordRepository skillKeywordRepository;
     private final RedisTemplate<String, Object> redisTemplate;
     private HashOperations<String, String, Set<String>> hashOperations;
 
-    /** 캐시 초기화 부분 **/
     @PostConstruct
-    public void initializeCache() {
+    public void init() {
         hashOperations = redisTemplate.opsForHash();
-        loadSkillsIntoCache();
-    }
-
-    private void loadSkillsIntoCache() {
-        List<Skill> skillList = skillRepository.findAll();
-        skillList.forEach(skill -> {
-            String keyword = skill.getKeyword().toLowerCase();
-            cacheKeyword(keyword);
-        });
-    }
-
-    private void cacheKeyword(String keyword) {
-        hashOperations.putIfAbsent(SKILL_KEYWORDS, keyword, new HashSet<>(Set.of(keyword)));
     }
 
     /** 캐시에서 데이터 제공하는 부분 **/
