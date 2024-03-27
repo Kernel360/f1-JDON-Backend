c107145d26cd4d1e9683f38db846f121aa8dcddb
aqrms <aquarimus@gmail.com>
2024-03-27 12:27:11 +0900

    Feature: 커피챗 만료처리 스케쥴링 적용 (#543)

---
+++ module-batch/src/main/java/kernel/jdon/modulebatch/domain/coffeechat/repository/CoffeeChatRepository.java
@@ -0,0 +1,12 @@
+package kernel.jdon.modulebatch.domain.coffeechat.repository;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import kernel.jdon.moduledomain.coffeechat.repository.CoffeeChatDomainRepository;
+
+public interface CoffeeChatRepository extends CoffeeChatDomainRepository {
+    @Modifying
+    @Query("update CoffeeChat c set c.coffeeChatStatus = 'CLOSE' where c.coffeeChatStatus <> 'CLOSE' and c.meetDate < CURRENT_TIMESTAMP")
+    void updateStatusToCloseForPastCoffeeChats();
+}
