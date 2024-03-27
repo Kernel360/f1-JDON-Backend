c22f2a98a0e4e9039a6a9eedf2e4b93ad61e56fb
이윤선 <yoonseon3@gmail.com>
2024-03-27 14:39:21 +0900

    Feature: 부분 원티드 JD 스크래핑 Step 슬랙 알림 설정

---
+++ module-batch/src/main/java/kernel/jdon/modulebatch/job/jd/listener/PartFrontWantedJobScrapingJStepListener.java
@@ -0,0 +1,43 @@
+package kernel.jdon.modulebatch.job.jd.listener;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+
+import kernel.jdon.modulecommon.slack.SlackSender;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@StepScope
+@RequiredArgsConstructor
+public class PartFrontWantedJobScrapingJStepListener implements StepExecutionListener {
+    private static final String STEP_NAME = "partFrontendWantedJdScrapingStep";
+
+    private final SlackSender slackSender;
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+        if (stepExecution.getStatus() == BatchStatus.STARTED) {
+            slackSender.sendStepStart(STEP_NAME);
+        }
+        log.info("[PartFrontWantedJobScrapingJStepListener #beforeJob] stepExecution is " + stepExecution.getStatus());
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        if (stepExecution.getStatus() == BatchStatus.FAILED) {
+            slackSender.sendStepError(STEP_NAME);
+            log.error("[PartFrontWantedJobScrapingJStepListener #afterJob] stepExecution is FAILED!!! RECOVER ASAP");
+        }
+        if (stepExecution.getStatus() == BatchStatus.COMPLETED) {
+            slackSender.sendStepEnd(STEP_NAME);
+
+        }
+        log.info("[PartFrontWantedJobScrapingJStepListener #afterJob] stepExecution is " + stepExecution.getStatus());
+
+        return new ExitStatus(stepExecution.getStatus().name());
+    }
+}
