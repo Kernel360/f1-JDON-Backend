fa8020bbab453741c0c4f4989bec21e9ac3d7e92
이윤선 <yoonseon3@gmail.com>
2024-04-14 04:57:42 +0900

    Chore: 미사용 원티드 스크래핑 API 제거

--- module-crawler/src/main/java/kernel/jdon/modulecrawler/legacy/skillhistory/repository/SkillHistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package kernel.jdon.modulecrawler.legacy.skillhistory.repository;
-
-import kernel.jdon.moduledomain.skillhistory.repository.SkillHistoryDomainRepository;
-
-public interface SkillHistoryRepository extends SkillHistoryDomainRepository {
-}
