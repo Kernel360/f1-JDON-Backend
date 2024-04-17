4186d7f74b252d5ce795ebd3bce1b66604d7c075
이윤선 <yoonseon3@gmail.com>
2024-04-17 20:37:19 +0900

    Test: 채용공고 스크래핑 따닥 예외반환 테스트 추가

--- module-crawler/src/test/java/kernel/jdon/modulecrawler/domain/jd/application/JdFacadeTest.java
+++ module-crawler/src/test/java/kernel/jdon/modulecrawler/domain/jd/application/JdFacadeTest.java
@@ -3,9 +3,13 @@ package kernel.jdon.modulecrawler.domain.jd.application;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import kernel.jdon.modulecrawler.domain.jd.core.JdService;
+import kernel.jdon.modulecrawler.domain.jd.error.JdErrorCode;
 import kernel.jdon.modulecrawler.global.exception.CrawlerException;
 
 @DisplayName("채용공고_스크래핑_따닥_테스트")
@@ -28,7 +33,7 @@ class JdFacadeTest {
 
     @DisplayName("멀티스레드 환경에서 다수의 요청이 들어와도 1개의 요청만 scrapeWantedJd 메서드의 락을 획득한다.")
     @Test
-    void 채용공고_스크래핑_동시성_테스트() throws InterruptedException {
+    void 채용공고_스크래핑_따닥_성공_개수_테스트() throws InterruptedException {
         // given
         final int threadCount = 10;
         final ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
@@ -55,5 +60,48 @@ class JdFacadeTest {
             () -> assertThat(successCount.get()).isEqualTo(1),
             () -> assertThat(failCount.get()).isEqualTo(9)
         );
+    }
+
+    @DisplayName("멀티스레드 환경에서 스레드1이 scrapeWantedJd를 실행중일 때 스레드2에서 scrapeWantedJd 메서드 실행 시 예외를 반환한다.")
+    @Test
+    void 채용공고_스크래핑_따닥_예외반환_테스트() throws InterruptedException {
+        // given
+        final int threadCount = 2;
+        final ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        final CountDownLatch latch = new CountDownLatch(threadCount);
+        final List<Future<?>> futures = new ArrayList<>();
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            futures.add(executorService.submit(() -> {
+                try {
+                    jdFacade.scrapeWantedJd();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    latch.countDown();
+                }
+            }));
+        }
+
+        CrawlerException crawlerException = null;
+        for (int i = 0; i < threadCount; i++) {
+            Future<?> future = futures.get(i);
+            try {
+                future.get(); // 스레드가 완료될 때까지 대기하고, 발생한 예외를 검사
+            } catch (ExecutionException e) {
+                Throwable cause = e.getCause();
+                if (cause.getCause() instanceof CrawlerException exception) {
+                    crawlerException = exception;
+                }
+            }
+        }
+
+        latch.await();
+        executorService.shutdown();
+
+        // then
+        assertThat(crawlerException.getErrorCode())
+            .isEqualTo(JdErrorCode.CONFLICT_FAIL_LOCK);
     }
 }
