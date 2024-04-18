c4e83fb44c679d704cdb9094b8fa5f3d08e8e4c3
이윤선 <yoonseon3@gmail.com>
2024-04-18 17:03:52 +0900

    Feature: crawler 모듈 실행 시 슬랙알림 설정

--- module-crawler/src/main/java/kernel/jdon/ModuleCrawlerApplication.java
+++ module-crawler/src/main/java/kernel/jdon/ModuleCrawlerApplication.java
@@ -1,8 +1,14 @@
 package kernel.jdon;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.stereotype.Component;
+
+import kernel.jdon.modulecommon.slack.SlackSender;
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -10,6 +16,23 @@ public class ModuleCrawlerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ModuleCrawlerApplication.class, args);
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    class ModuleCrawlerApplicationRunner implements ApplicationRunner {
+        private static final String MODULE_NAME = "module-crawler";
+
+        private final SlackSender slackSender;
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            sendServerStart();
+        }
+
+        private void sendServerStart() {
+            slackSender.sendServerStart(MODULE_NAME);
+        }
     }
 
 }
